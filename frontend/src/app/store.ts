40d4d8db21bc1686549700634a4a24952e079355
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-11 02:54:53 +0200

    Frontend: Added the charplanner reducer to the store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from './api/apiSlice';
+import charplannerReducer from "../features/charplanner/charplannerSlice";
 
 export const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
+        charplanner: charplannerReducer
     },
     middleware: getDefaultMiddleware =>
         getDefaultMiddleware().concat(apiSlice.middleware),
