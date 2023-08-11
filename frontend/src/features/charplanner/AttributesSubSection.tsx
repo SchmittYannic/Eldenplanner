cf9f90a17972ef3a004c5110159c5e753bbef4bb
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-11 17:14:43 +0200

    Frontend: Updated AttributesSubSection
    Only total stat calculation remains unfinished.

--- frontend/src/features/charplanner/AttributesSubSection.tsx
+++ frontend/src/features/charplanner/AttributesSubSection.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { ReactElement } from "react";
+import { useSelector } from "react-redux";
+import { StatsType, StartingClassData } from "../../../data/StartingClassData";
+import {
+    selectStartingclass,
+    selectorsMap,
+    reduceractionsMap
+} from "./charplannerSlice";
+import AttributeRow from "./AttributeRow";
+import RuneDisplayWrapper from "./RuneDisplayWrapper";
 
-const AttributesSubSection = () => {
+const AttributesSubSection = (): ReactElement => {
+
+    const startingclass = useSelector(selectStartingclass);
+
+    const minStats = StartingClassData[startingclass];
+
     return (
-        <div>AttributesSubSection</div>
+        <div className="AttributesSubSection">
+            <RuneDisplayWrapper>
+                <div className="AttributeRow">
+                    <span>Stat</span>
+                    <span>Init</span>
+                    <span>Level</span>
+                    <span>Total</span>
+                    <span></span>
+                </div>
+                <>
+                    {Object.keys(minStats).map((keyName, idx) => (
+                        <AttributeRow 
+                            key={idx} 
+                            keyName={keyName as keyof StatsType}
+                            minStat={minStats[keyName as keyof StatsType]}
+                            statSelector={selectorsMap[keyName as keyof StatsType]}
+                            statAction={reduceractionsMap[keyName as keyof StatsType]}
+                        />
+                    ))}
+                </>
+            </RuneDisplayWrapper>
+        </div>
     )
 }
 
---
+++ frontend/src/features/charplanner/RuneDisplayWrapper.tsx
@@ -0,0 +1,77 @@
+import { ReactElement } from "react";
+import { useSelector } from "react-redux";
+import {
+    calcRuneLevel,
+    calcNextLevelRunes,
+    calcTotalRunesSpend,
+    addThousandsSeperator
+} from "../../utils/functions";
+import {
+    selectStartingclass,
+    selectVigor,
+    selectMind,
+    selectEndurance,
+    selectStrength,
+    selectDexterity,
+    selectIntelligence,
+    selectFaith,
+    selectArcane
+} from "./charplannerSlice";
+import StartingClassData from "../../../data/StartingClassData";
+
+const RuneDisplayWrapper = ({ children }: {children: ReactElement[]}) => {
+
+    const startingclass = useSelector(selectStartingclass);
+
+    const vigor = useSelector(selectVigor);
+    const mind = useSelector(selectMind);
+    const endurance = useSelector(selectEndurance);
+    const strength = useSelector(selectStrength);
+    const dexterity = useSelector(selectDexterity);
+    const intelligence = useSelector(selectIntelligence);
+    const faith = useSelector(selectFaith);
+    const arcane = useSelector(selectArcane);
+
+    const startingStats = StartingClassData[startingclass];
+
+    const startLevel = calcRuneLevel(startingStats);
+
+    const runeLevel = calcRuneLevel({
+        vigor,
+        mind,
+        endurance,
+        strength,
+        dexterity,
+        intelligence,
+        faith,
+        arcane
+    });
+
+    const nextLevelRunes = calcNextLevelRunes(runeLevel);
+    const totalRunesSpend = calcTotalRunesSpend(startLevel, runeLevel);
+
+    const nextLevelRunesString = addThousandsSeperator(nextLevelRunes);
+    const totalRunesSpendString = addThousandsSeperator(totalRunesSpend);
+
+    return (
+        <>
+            <div className="RuneLevel">
+                <span>Level</span>
+                <span>{runeLevel}</span>
+            </div>
+            { ...children}
+            <div className="RuneCost">
+                <div>
+                    <span>Total Runes</span>
+                    <span>{totalRunesSpendString}</span>
+                </div>
+                <div>
+                    <span>To Next Level</span>
+                    <span>{nextLevelRunesString}</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default RuneDisplayWrapper
