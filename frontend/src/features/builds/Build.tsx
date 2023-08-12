a90af82415341d09b152e53306a532302890fcd6
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-12 22:24:37 +0200

    Frontend: Updated BuildsList
    Now displays a table with all the saved builds from the database.

---
+++ frontend/src/features/builds/Build.tsx
@@ -0,0 +1,45 @@
+import { ReactElement } from "react";
+import { EntityId } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
+import { BuildType, useGetBuildsQuery } from "./buildsApiSlice";
+import { UserType, selectUserById } from "../users/usersApiSlice";
+import { RootState } from "../../app/store";
+import { calcSumObjectValues } from "../../utils/functions";
+
+type PropsType = {
+    buildId: EntityId
+}
+
+const Build = ({ buildId }: PropsType): ReactElement => {
+
+    const data = useGetBuildsQuery("buildsList", {
+        selectFromResult: ({ data }) => ({
+            build: data?.entities[buildId]
+        }),
+    });
+
+    const build = data.build as BuildType;
+
+    if (build) {
+        const user = useSelector((state: RootState) => selectUserById(state, build.user as EntityId)) as UserType;
+
+        const sumStats = calcSumObjectValues(build.stats);
+
+        const runelevel = sumStats - 80;
+
+        const createdAt = new Date(build.createdAt);
+        const updatedAt = new Date(build.updatedAt);
+        return (
+            <tr className="table__row user">
+                <td className={`table__cell`}>{build.general.charactername}</td>
+                <td className={`table__cell`}>{user ? user.username : "Unknown"}</td>
+                <td className={`table__cell`}>{runelevel}</td>
+                <td className={`table__cell`}>0</td>
+                <td className={`table__cell`}>{createdAt.toLocaleString()}</td>
+                <td className={`table__cell`}>{updatedAt.toLocaleString()}</td>
+            </tr>
+        )
+    } else return <></>
+}
+
+export default Build
--- frontend/src/features/builds/BuildsList.tsx
+++ frontend/src/features/builds/BuildsList.tsx
@@ -1,11 +1,73 @@
-import { ReactElement} from "react"
+import { ReactElement} from "react";
+import { useGetBuildsQuery } from "./buildsApiSlice";
+import Build from "./Build";
 
 const BuildsList = (): ReactElement => {
-    return (
-        <main>
-            <h2>BuildsList</h2>
-        </main>
-    )
+    const {
+        data: builds,
+        isLoading,
+        isSuccess,
+        isError,
+        error     
+    } = useGetBuildsQuery("buildsList");
+
+    let content: ReactElement = (<></>);
+
+    if (isError) {
+        let errorMsg = "";
+
+        if ("data" in error) {
+            errorMsg = error?.data?.message;
+        } else {
+            errorMsg = "An Error occured";
+        }
+
+        content = (
+            <main>
+                <p className="error">
+                    {errorMsg}
+                </p>
+            </main>
+        )
+    }
+
+    if (isLoading) {
+        content = (
+            <main>
+                <p>
+                    Loading...
+                </p>
+            </main>
+        )
+    }
+
+    if (isSuccess) {
+        const { ids } = builds;
+
+        const tableContent = ids?.length && ids.map((buildId) => <Build key={buildId} buildId={buildId}/>)
+
+        content = (
+            <main>
+                <table className="table table--builds">
+                    <thead className="table__thead">
+                        <tr className="table__row">
+                            <th scope="col" className="table__th build__charactername">Charactername</th>
+                            <th scope="col" className="table__th build__user">Author</th>
+                            <th scope="col" className="table__th build__runelevel">Level</th>
+                            <th scope="col" className="table__th build__stars">Stars</th>
+                            <th scope="col" className="table__th user__createdAt">CreatedAt</th>
+                            <th scope="col" className="table__th user__updatedAt">UpdatedAt</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tableContent}
+                    </tbody>
+                </table>
+            </main>
+        )
+    }
+
+    return content
 }
 
 export default BuildsList
