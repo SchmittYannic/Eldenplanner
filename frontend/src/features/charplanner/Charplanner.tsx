db0975eb5fff6649032c54925dc60cbb63d534ff
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-13 00:29:47 +0200

    Frontend: Load Build in Charplanner
    Using optional param to load a build into the charplanner.
    
    If param exists the id is used to get the entire build from the prefetched buildsList.
    
    The build is then dispatched to the charplannerSlice and set as charplanner state.

--- frontend/src/features/charplanner/Charplanner.tsx
+++ frontend/src/features/charplanner/Charplanner.tsx
@@ -1,4 +1,11 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import { BuildType, selectBuildById } from "../builds/buildsApiSlice";
+import { RootState } from "../../app/store";
+import { CharplannerStateType, loadBuild } from "./charplannerSlice";
+
 import CharacterSection from "./CharacterSection";
 import EquipmentSection from "./EquipmentSection";
 import InfoSection from "./InfoSection";
@@ -6,6 +13,41 @@ import ActionsSection from "./ActionsSection";
 import "./Charplanner.scss";
 
 const Charplanner = (): ReactElement => {
+
+    const dispatch = useDispatch();
+    const param = useParams();
+    const buildRef = useRef<CharplannerStateType>();
+
+    if (param?.buildId) {
+        // if param and buildId exist
+        const buildId = param.buildId;
+        // select the build using its id from the param
+        const build = useSelector((state: RootState) => selectBuildById(state, buildId)) as BuildType;
+
+        if (build) {
+            // if a build was successfully selected save it to the buildRef
+            buildRef.current = {
+                general: build.general,
+                stats: build.stats,
+                armament: build.armament,
+                talisman: build.talisman,
+                armor: build.armor
+            };
+        }
+    }
+
+    /*
+        whenever the buildRef changes check if it isnt undefined 
+        in case it isnt use its value to load a build done in 
+        useEffect so dispatch is only getting triggered after 
+        component mounted.
+    */
+    useEffect(() => {
+        if (buildRef.current) {
+            dispatch(loadBuild(buildRef.current));
+        }
+    }, [buildRef.current]);
+
     return (
         <main className="Charplanner">
             <CharacterSection />
