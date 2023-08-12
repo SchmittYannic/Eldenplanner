06e83b42d7dacb42e5f8c3805542f34e0038211f
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-12 22:53:34 +0200

    Frontend: Update CollapsibleList
    Change bottom corner radii, when items list is empty.

--- frontend/src/components/ui/CollapsibleList.tsx
+++ frontend/src/components/ui/CollapsibleList.tsx
@@ -8,12 +8,16 @@ type PropsType = {
 
 const CollapsibleList = ({ heading, items }: PropsType): ReactElement => {
     const [active, setActive] = useState(true);
+
+    const emptyListStyle = !items.length ? { borderBottomLeftRadius: ".5em", borderBottomRightRadius: ".5em" } : {}
+
     return (
         <div className="CollapsibleList">
             <button 
                 className={active ? "CollButton Active" : "CollButton"}
                 tabIndex={0}
                 onClick={() => setActive(!active)}
+                style={emptyListStyle}
             >
                 {heading}
             </button>
