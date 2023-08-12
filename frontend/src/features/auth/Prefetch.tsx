f67e48a9c84fefb1e96b53abd611761b3bac62e5
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-13 00:22:13 +0200

    Frontend: Prefetching buildsList

--- frontend/src/features/auth/Prefetch.tsx
+++ frontend/src/features/auth/Prefetch.tsx
@@ -2,11 +2,13 @@ import { ReactElement, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import { store } from "../../app/store";
 import { usersApiSlice } from "../users/usersApiSlice";
+import { buildsApiSlice } from "../builds/buildsApiSlice";
 
 const Prefetch = (): ReactElement => {
 
     useEffect(() => {
         store.dispatch(usersApiSlice.util.prefetch("getUsers", "usersList", { force: true }));
+        store.dispatch(buildsApiSlice.util.prefetch("getBuilds", "buildsList", { force: true }));
     }, []);
 
     return <Outlet />
