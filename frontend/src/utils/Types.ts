46ebdf7e662144d946fd11a3bc419803c60cc0fb
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-12 19:34:52 +0200

    Frontend: Added Types.ts in utils
    To save types, that dont belong to one particular file.

---
+++ frontend/src/utils/Types.ts
@@ -0,0 +1,8 @@
+export type EquipedWeaponsType = {
+    lefthand1Weapon: string,
+    lefthand2Weapon: string,
+    lefthand3Weapon: string,
+    righthand1Weapon: string,
+    righthand2Weapon: string,
+    righthand3Weapon: string,
+}
