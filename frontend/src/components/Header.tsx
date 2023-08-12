3e91668b76073a1a1f80c75b842095a490f707a9
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-12 22:35:43 +0200

    Frontend: Added Links to Header for testing purposes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,9 +1,29 @@
 import { ReactElement } from "react";
+import { Link } from "react-router-dom";
 
 const Header = (): ReactElement => {
+
     return (
         <header>
-            <h3>This is the header</h3>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/builds">
+                            Builds
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/users">
+                            Users
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/charplanner">
+                            Charplanner
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
