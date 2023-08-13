669dd3ed6d34b80b7fa4a2b1aebaa7db73a9e33f
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-13 20:07:43 +0200

    Backend: Insert Middleware before createNewBuild and updateBuild
    Received request data will be validated through checkBuildData middleware before the request hits createNewBuild or updateBuild.

--- backend/routes/buildRoutes.js
+++ backend/routes/buildRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getAllBuilds, createNewBuild, updateBuild, deleteBuild } from "../controllers/buildsControllers.js";
+import checkBuildData from "../middleware/checkBuildData.js";
 const router = express.Router();
 
 router.route("/")
     .get(getAllBuilds)
-    .post(createNewBuild)
-    .patch(updateBuild)
+    .post(checkBuildData, createNewBuild)
+    .patch(checkBuildData, updateBuild)
     .delete(deleteBuild);
 
 export default router;
