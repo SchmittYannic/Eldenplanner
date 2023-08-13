61af579ee4971d8027f20b6203d50349d986a0c6
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-13 22:31:48 +0200

    Frontend: Updated Login page
    Added some basic elements to the login page to test functionality later.

--- frontend/src/features/auth/Login.tsx
+++ frontend/src/features/auth/Login.tsx
@@ -1,8 +1,63 @@
-import { ReactElement } from "react";
+import { ChangeEvent, KeyboardEvent, MouseEvent, ReactElement, useState } from "react";
 
 const Login = (): ReactElement => {
+
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [responseMsg, setResponseMsg] = useState("");
+
+    const onUserChange = (e: ChangeEvent<HTMLInputElement>) => setUser(e.target.value);
+    const onPasswordChange = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value);
+
+    const onSubmitClicked = (e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+    };
+
+    const responseMsgClass = "";
+
     return (
-        <h1>Login</h1>
+        <main>
+            <h1>Login</h1>
+            <form
+                className="form--login"
+                method="post"
+            >
+                <label htmlFor="signup-user">
+                    Username or Email
+                </label>
+                <input
+                    id="signup-user"
+                    type="text"
+                    value={user}
+                    onChange={onUserChange}
+                    autoComplete="off"
+                />
+
+                <label htmlFor="signup-password">
+                    Password
+                </label>
+                <input
+                    id="signup-password"
+                    type="password"
+                    value={password}
+                    onChange={onPasswordChange}
+                    autoComplete="off"
+                />
+
+                <p className={`msg--login ${responseMsgClass}`}>
+                    {responseMsg}
+                </p>
+
+                <button
+                    className="btn"
+                    type="submit"
+                    onClick={onSubmitClicked}
+                >
+                    Login
+                </button>
+            </form>
+        </main>
     )
 };
 
