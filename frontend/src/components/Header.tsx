9f73009bec7c8ba37ff7949c895fbc1899793ede
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-14 21:09:58 +0200

    Frontend: Using authorization on header
    Example usage of authorization on header component.
    
    Link to users route only rendered for Admins and Demoadmins.
    
    Logout button and username only rendered for logged in users.
    
    Signup and login Links only rendered for visitors of website.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,13 @@
 import { ReactElement } from "react";
 import { Link } from "react-router-dom";
+import { useSendLogoutMutation } from "../features/auth/authApiSlice";
+import useAuth from "../hooks/useAuth";
 
 const Header = (): ReactElement => {
+
+    const { username, status, isUser, isDemoadmin, isAdmin } = useAuth();
+
+    const [sendLogout, {}] = useSendLogoutMutation();
 
     return (
         <header>
@@ -12,26 +18,48 @@ const Header = (): ReactElement => {
                             Builds
                         </Link>
                     </li>
-                    <li>
-                        <Link to="/users">
-                            Users
-                        </Link>
-                    </li>
+                    {(isAdmin || isDemoadmin) && (
+                        <li>
+                            <Link to="/users">
+                                Users
+                            </Link>
+                        </li>
+                    )}
                     <li>
                         <Link to="/charplanner">
                             Charplanner
                         </Link>
                     </li>
-                    <li>
-                        <Link to="/signup">
-                            Sign Up
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/login">
-                            Login
-                        </Link>
-                    </li>
+                    {status === "Visitor" && (
+                        <li>
+                            <Link to="/signup">
+                                Sign Up
+                            </Link>
+                        </li>
+                    )}
+                    {status === "Visitor" && (
+                        <li>
+                            <Link to="/login">
+                                Login
+                            </Link>
+                        </li>
+                    )}
+                    {status !== "Visitor" && (
+                        <li>
+                            <button
+                                type="button"
+                                onClick={sendLogout}
+                                style={{ backgroundColor: "white"}}
+                            >
+                                Logout
+                            </button>
+                        </li>
+                    )}
+                    {(isUser || isDemoadmin || isAdmin) && (
+                        <li>
+                            <p>{username}</p>
+                        </li>
+                    )}
                 </ul>
             </nav>
         </header>
