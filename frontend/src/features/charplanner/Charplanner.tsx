e4b93a4fc46854899bd09c4fc3a4430af6fd49a7
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-18 22:27:31 +0200

    Frontend: Fixed Charplanner component
    When visiting a build directly through the link it would cause an error.
    
    This was caused because the called hook changed between render.
    
    Hooks in react shouldnt be used conditionally, therefore the code was rewritten to keep its functionality by moving the conditions inside of the callback functions in the useSelector hooks.

--- frontend/src/features/charplanner/Charplanner.tsx
+++ frontend/src/features/charplanner/Charplanner.tsx
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { BuildType, selectBuildById } from "../builds/buildsApiSlice";
 import { RootState } from "../../app/store";
 import { CharplannerStateType, loadBuild, resetCharplanner } from "./charplannerSlice";
-import { UserType, selectUserById } from "../users/usersApiSlice";
+import { selectUserById, UserType } from "../users/usersApiSlice";
 
 import CharacterSection from "./CharacterSection";
 import EquipmentSection from "./EquipmentSection";
@@ -18,31 +18,35 @@ const Charplanner = (): ReactElement => {
     const dispatch = useDispatch();
     const param = useParams();
     const buildRef = useRef<CharplannerStateType>();
-    
-    let buildAuthor: string = "";
 
-    if (param?.buildId) {
-        // if param and buildId exist
-        const buildId = param.buildId;
-        // select the build using its id from the param
-        const build = useSelector((state: RootState) => selectBuildById(state, buildId)) as BuildType;
-
-        const { username } = useSelector((state: RootState) => selectUserById(state, build.user) as UserType);
-
-        buildAuthor = username;
-
-        if (build) {
-            // if a build was successfully selected save it to the buildRef
-            buildRef.current = {
-                general: build.general,
-                stats: build.stats,
-                armament: build.armament,
-                talisman: build.talisman,
-                armor: build.armor
-            };
+    // if param exist select Build through buildId in param
+    const build = useSelector((state: RootState) => {
+        if (param?.buildId) {
+            return selectBuildById(state, param.buildId) as BuildType
         }
+        return null
+    });
+
+    // if build exists select builds author
+    const buildAuthor = useSelector((state: RootState) => {
+        if(build?.user) {
+            return selectUserById(state, build.user) as UserType
+        }
+        return null
+    });
+
+    if (build) {
+        // if a build was successfully selected save it to the buildRef
+        buildRef.current = {
+            general: build.general,
+            stats: build.stats,
+            armament: build.armament,
+            talisman: build.talisman,
+            armor: build.armor
+        };
     }
 
+    // on mount check if buildRef is undefined. If yes reset charplannerSlice.
     useEffect(() => {
         if (!buildRef.current) {
             dispatch(resetCharplanner());
@@ -63,7 +67,7 @@ const Charplanner = (): ReactElement => {
 
     return (
         <main>
-            {buildAuthor && <h2>{buildAuthor}</h2>}
+            {buildAuthor && <h2>{buildAuthor.username}</h2>}
             <div className="Charplanner">
                 <CharacterSection />
                 <EquipmentSection />
