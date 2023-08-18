7d1b63dca6c3d5ac9d51d70908c686a918161672
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-18 18:00:38 +0200

    Frontend: Create usePersist hook
    Hook looks at the value of persist in local storage.
    
    Creates a state called persist with the defaul value of false, if nothing was found in local storage. If persist exists in local storage it becomes the default of the persist state.
    
    useEffect, which writes changes to persist immediately into the local storage.
    
    hook returns both the persist state and the dispatch function.

---
+++ frontend/src/hooks/usePersist.ts
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react"
+
+const usePersist = (): [boolean, React.Dispatch<React.SetStateAction<boolean>>] => {
+    const persistInLocalStorage: string | null = localStorage.getItem("persist");
+
+    const [persist, setPersist] = useState<boolean>(typeof persistInLocalStorage === "string" ? JSON.parse(persistInLocalStorage) : false);
+
+    useEffect(() => {
+        localStorage.setItem("persist", JSON.stringify(persist));
+    }, [persist]);
+
+    return [persist, setPersist]
+}
+export default usePersist
