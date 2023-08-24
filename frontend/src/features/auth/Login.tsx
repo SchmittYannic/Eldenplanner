bdca5330a696f2dde988303aad40cf065b80eeb9
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-24 20:23:57 +0200

    Frontend: Adding className to error msg in Login page

--- frontend/src/features/auth/Login.tsx
+++ frontend/src/features/auth/Login.tsx
@@ -121,7 +121,7 @@ const Login = (): ReactElement => {
                             <div className="divider-4" />
 
                             {isError && (
-                                <div className="errmsg">
+                                <div className="sm-alert errmsg">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                                     <span>{responseMsg}</span>
                                 </div>
