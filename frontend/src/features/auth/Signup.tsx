5871637512422979aa808b10986e8d15fbcdc41e
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-24 20:23:37 +0200

    Frontend: Adding success msg to Signup page

--- frontend/src/features/auth/Signup.tsx
+++ frontend/src/features/auth/Signup.tsx
@@ -133,8 +133,16 @@ const Signup = (): ReactElement => {
                             {/* <p className={`msg--signup ${responseMsgClass}`}>
                                 {responseMsg}
                             </p> */}
+
+                            {isSuccess && (
+                                <div className="sm-alert succmsg">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                    <span>{responseMsg}</span>
+                                </div>
+                            )}
+
                             {isError && (
-                                <div className="errmsg">
+                                <div className="sm-alert errmsg">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                                     <span>{responseMsg}</span>
                                 </div>
