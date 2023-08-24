1b9fcb3edc733c806f64d6762e1a939ae9b6e795
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-24 18:26:30 +0200

    Frontend: Added Assets signupimg
    from:
    https://www.wallpaperflare.com/elden-ring-playstation-wallpaper-yzycw

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,5 +1,13 @@
 import loginimg from "./loginimg.jpg";
+import signupimg420w from "./signupimg420w.jpg";
+import signupimg980w from "./signupimg980w.jpg";
+import signupimg1680w from "./signupimg1680w.jpg";
+import signupimg from "./signupimg.jpg";
 
 export {
     loginimg,
+    signupimg420w,
+    signupimg980w,
+    signupimg1680w,
+    signupimg,
 }
