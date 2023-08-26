984bb1cfefd9971595a802a5d513155e5c84ba6f
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-26 19:12:26 +0200

    Frontend: Created usersAsAdminApiSlice
    Moved userAsAdminType and getUsersAsAdmin endpoint from usersApiSlice to usersAsAdminApiSlice.
    
    Created updateUserAsAdmin endpoint.

--- frontend/src/features/users/usersApiSlice.ts
+++ frontend/src/features/users/usersApiSlice.ts
@@ -6,28 +6,16 @@ import {
 import { apiSlice } from "../../app/api/apiSlice"
 import { RootState } from "../../app/store";
 
-const usersAdapter = createEntityAdapter({})
+const usersAdapter = createEntityAdapter({});
 
-const initialState = usersAdapter.getInitialState()
+const initialState = usersAdapter.getInitialState();
 
 export type UserType = {
     _id: number
     id: number
     username: string
     createdAt: string
-}
-
-export type UserAsAdminType = {
-    _id: number
-    id: number
-    username: string
-    email: string
-    validated: boolean
-    active: boolean
-    roles: string[]
-    createdAt: string
-    updatedAt: string
-}
+};
 
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: builder => ({
@@ -93,28 +81,6 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 { type: 'User', id: arg.id }
             ]
         }),
-        getUsersAsAdmin: builder.query<EntityState<unknown>, string>({
-            query: () => ({
-                url: '/users/admin',
-                validateStatus: (response, result) => {
-                    return response.status === 200 && !result.isError
-                },
-            }),
-            transformResponse: (responseData: UserAsAdminType[]) => {
-                const loadedUsers = responseData.map(user => {
-                    user.id = user._id
-                    return user
-                });
-                return usersAdapter.setAll(initialState, loadedUsers)         
-            },
-            providesTags: (result) =>
-                result
-                ? [
-                    ...result.ids.map(( id ) => ({ type: 'User' as const, id })),
-                    { type: 'User', id: 'LIST' },
-                    ]
-                : [{ type: 'User', id: 'LIST' }],
-            }),
     }),
 })
 
@@ -123,12 +89,10 @@ export const {
     useAddNewUserMutation,
     useUpdateUserMutation,
     useDeleteUserMutation,
-    useGetUsersAsAdminQuery,
 } = usersApiSlice
 
 // returns the query result object
 export const selectUsersResult = usersApiSlice.endpoints.getUsers.select("usersList");
-export const selectUsersAsAdminResult = usersApiSlice.endpoints.getUsersAsAdmin.select("usersList");
 
 //creates memoized selector
 const selectUsersData = createSelector(
@@ -136,22 +100,10 @@ const selectUsersData = createSelector(
     usersResult => usersResult.data // normalized state object with ids & entities
 );
 
-const selectUsersAsAdminData = createSelector(
-    selectUsersAsAdminResult,
-    usersResult => usersResult.data // normalized state object with ids & entities
-)
-
 //getSelectors creates these selectors and we rename them with aliases using destructuring
 export const {
     selectAll: selectAllUsers,
     selectById: selectUserById,
     selectIds: selectUserIds
     // Pass in a selector that returns the users slice of state
-} = usersAdapter.getSelectors((state: RootState) => selectUsersData(state) ?? initialState)
-
-export const {
-    selectAll: selectAllUsersAsAdmin,
-    selectById: selectUserByIdAsAdmin,
-    selectIds: selectUserIdsAsAdmin
-    // Pass in a selector that returns the users slice of state
-} = usersAdapter.getSelectors((state: RootState) => selectUsersAsAdminData(state) ?? initialState)
+} = usersAdapter.getSelectors((state: RootState) => selectUsersData(state) ?? initialState);
---
+++ frontend/src/features/users/usersAsAdminApiSlice.ts
@@ -0,0 +1,87 @@
+import {
+    createEntityAdapter,
+    EntityState,
+    createSelector,
+} from "@reduxjs/toolkit";
+import { apiSlice } from "../../app/api/apiSlice";
+import { RootState } from "../../app/store";
+
+const usersAsAdminAdapter = createEntityAdapter({});
+
+const initialState = usersAsAdminAdapter.getInitialState();
+
+export type UserAsAdminType = {
+    _id: number
+    id: number
+    username: string
+    email: string
+    validated: boolean
+    active: boolean
+    roles: string[]
+    createdAt: string
+    updatedAt: string
+};
+
+export const usersAsAdminApiSlice = apiSlice.injectEndpoints({
+    endpoints: builder => ({
+        getUsersAsAdmin: builder.query<EntityState<unknown>, string>({
+            query: () => ({
+                url: "/users/admin",
+                validateStatus: (response, result) => {
+                    return response.status === 200 && !result.isError
+                },
+            }),
+            transformResponse: (responseData: UserAsAdminType[]) => {
+                const loadedUsers = responseData.map(user => {
+                    user.id = user._id
+                    return user
+                });
+                return usersAsAdminAdapter.setAll(initialState, loadedUsers)         
+            },
+            providesTags: (result) =>
+                result
+                ? [
+                    ...result.ids.map(( id ) => ({ type: "User" as const, id })),
+                    { type: "User", id: "LIST" },
+                    ]
+                : [{ type: "User", id: "LIST" }],
+            }),
+        updateUserAsAdmin: builder.mutation({
+            query: initialUserData => ({
+                url: "/users/admin",
+                method: "PATCH",
+                body: {
+                    ...initialUserData,
+                },
+                validateStatus: (response, result) => {
+                    return response.status === 200 && !result.isError
+                },
+            }),
+            invalidatesTags: (arg) => [
+                { type: "User", id: arg.id }
+            ]
+        }),
+    }),
+});
+
+export const {
+    useGetUsersAsAdminQuery,
+    useUpdateUserAsAdminMutation,
+} = usersAsAdminApiSlice;
+
+// returns the query result object
+export const selectUsersAsAdminResult = usersAsAdminApiSlice.endpoints.getUsersAsAdmin.select("usersList");
+
+//creates memoized selector
+const selectUsersAsAdminData = createSelector(
+    selectUsersAsAdminResult,
+    usersResult => usersResult.data // normalized state object with ids & entities
+);
+
+//getSelectors creates these selectors and we rename them with aliases using destructuring
+export const {
+    selectAll: selectAllUsersAsAdmin,
+    selectById: selectUserByIdAsAdmin,
+    selectIds: selectUserIdsAsAdmin
+    // Pass in a selector that returns the users slice of state
+} = usersAsAdminAdapter.getSelectors((state: RootState) => selectUsersAsAdminData(state) ?? initialState)
