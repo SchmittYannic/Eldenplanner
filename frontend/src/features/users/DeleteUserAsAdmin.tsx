cfce2f6d3ef0f93aa18c86ddd478fe592e2ab37e
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-30 03:39:23 +0200

    Frontend: Finished DeleteUserAsAdmin

--- frontend/src/features/users/DeleteUserAsAdmin.tsx
+++ frontend/src/features/users/DeleteUserAsAdmin.tsx
@@ -1,6 +1,8 @@
 import { ReactElement,useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 import { Alert } from "../../components/ui";
-import { UserAsAdminType } from "./usersAsAdminApiSlice";
+import { UserAsAdminType, useDeleteUserAsAdminMutation } from "./usersAsAdminApiSlice";
 
 type PropsType = {
     user: UserAsAdminType,
@@ -9,7 +11,30 @@ type PropsType = {
 
 const DeleteUserAsAdmin = ({ user, setTrigger }: PropsType): ReactElement => {
 
+    const [DeleteUserAsAdmin, {
+        isLoading,
+        isError,
+    }] = useDeleteUserAsAdminMutation();
+
+    const navigate = useNavigate();
+
     const [inputValue, setInputValue] = useState("");
+    const [responseMsg, setResponseMsg] = useState("");
+
+    const onConfirmDeletionClicked = async () => {
+        try {
+            await DeleteUserAsAdmin(user).unwrap();
+            navigate("/users")
+        } catch (err: any) {
+            if (!err.status) {
+                setResponseMsg("No Server Response");
+            } else if (err.status === 400 || err.status === 401) {
+                setResponseMsg(err.data?.message);
+            } else {
+                setResponseMsg("an error occured");
+            }
+        }
+    };
 
     const isUsername = inputValue === user.username;
 
@@ -29,23 +54,33 @@ const DeleteUserAsAdmin = ({ user, setTrigger }: PropsType): ReactElement => {
                         maxLength={30}
                         value={inputValue}
                         onChange={(e) => setInputValue(e.target.value)}
+                        autoComplete="off"
                     />
                 </div>
+
+                {isError && (
+                    <div className="sm-alert errmsg">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                        <span>{responseMsg}</span>
+                    </div>
+                )}
+
                 <button
                     className="action-btn"
                     type="submit"
+                    onClick={onConfirmDeletionClicked}
                     disabled={!isUsername}
                 >
-                    Confirm Deletion
+                    {!isLoading ? "Confirm Deletion" :
+                        <ClipLoader
+                            color={"rgb(231, 214, 182)"}
+                            loading={isLoading}
+                            size={20}
+                            aria-label="Loading Spinner"
+                            data-testid="loader"
+                        />
+                    }
                 </button>
-                {/* <button
-                    className="action-btn"
-                    type="button"
-                    onClick={isBuildAuthor ? onUpdateBuildClicked : onSaveBuildClicked}
-                >
-                    {isBuildAuthor ? "Update" : "Save"}
-                </button>
-                {(isSaveLoading || isUpdateLoading) &&  <p>is Loading...</p>} */}
             </div>
         </Alert>
     )
