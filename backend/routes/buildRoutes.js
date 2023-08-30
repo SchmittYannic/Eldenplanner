1efc0fb678709213789cea11233468fb6e0c2c90
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-30 04:10:21 +0200

    Backend: Using verifyJWT for buildRoutes

--- backend/routes/buildRoutes.js
+++ backend/routes/buildRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { getAllBuilds, createNewBuild, updateBuild, deleteBuild } from "../controllers/buildsController.js";
 import checkBuildData from "../middleware/checkBuildData.js";
+import verifyJWT from "../middleware/verifyJWT.js";
 const router = express.Router();
 
 router.route("/")
     .get(getAllBuilds)
-    .post(checkBuildData, createNewBuild)
-    .patch(checkBuildData, updateBuild)
-    .delete(deleteBuild);
+    .post(verifyJWT, checkBuildData, createNewBuild)
+    .patch(verifyJWT, checkBuildData, updateBuild)
+    .delete(verifyJWT, deleteBuild);
 
 export default router;
