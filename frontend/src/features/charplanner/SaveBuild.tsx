ba401f62b6fbd50de8323a2ada3ee4fa5375d45b
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-30 04:11:02 +0200

    Frontend: Update SaveBuild with ClipLoader and error messages

--- frontend/src/features/charplanner/SaveBuild.tsx
+++ frontend/src/features/charplanner/SaveBuild.tsx
@@ -1,6 +1,7 @@
-import { ChangeEvent, ReactElement, useEffect, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { ClipLoader } from "react-spinners";
 import { useAddNewBuildMutation, useUpdateBuildMutation } from "./charplannerApiSlice";
 import { selectCharplannerData } from "./charplannerSlice";
 import { Alert } from "../../components/ui";
@@ -22,14 +23,12 @@ const SaveBuild = ({ setTrigger }: PropsType): ReactElement => {
         isLoading: isSaveLoading,
         isSuccess: isSaveSuccess,
         isError: isSaveError,
-        error: saveerror
     }] = useAddNewBuildMutation();
 
     const [updateBuild, {
         isLoading: isUpdateLoading,
         isSuccess: isUpdateSuccess,
         isError: isUpdateError,
-        error: updateerror
     }] = useUpdateBuildMutation();
 
     const build = useSelector((state: RootState) => {
@@ -41,17 +40,35 @@ const SaveBuild = ({ setTrigger }: PropsType): ReactElement => {
     const isBuildAuthor = userId === build?.user;
     const initialStateTextarea = isBuildAuthor ? build?.title : "";
     const [textareaInput, setTextareaInput] = useState(initialStateTextarea);
+    const [responseMsg, setResponseMsg] = useState("");
 
-    const onTextareaChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        setTextareaInput(e.target.value);
-    };
+    const buttonText = isBuildAuthor ? "Update" : "Save";
 
     const onSaveBuildClicked = async () => {
-        await addNewBuild({ userId, title: textareaInput, data: charplannerData })
-    };
-
-    const onUpdateBuildClicked = async () => {
-        await updateBuild({ buildId: param.buildId, userId, title: textareaInput, data: charplannerData })
+        try {
+            if (isBuildAuthor) {
+                await updateBuild({
+                    buildId: param.buildId,
+                    userId,
+                    title: textareaInput,
+                    data: charplannerData
+                }).unwrap();
+            } else {
+                await addNewBuild({
+                    userId,
+                    title: textareaInput,
+                    data: charplannerData
+                }).unwrap();
+            }
+        } catch (err: any) {
+            if (!err.status) {
+                setResponseMsg("No Server Response");
+            } else if (err.status === 400 || err.status === 401) {
+                setResponseMsg(err.data?.message);
+            } else {
+                setResponseMsg("an error occured");
+            }
+        }
     };
 
     useEffect(() => {
@@ -74,16 +91,31 @@ const SaveBuild = ({ setTrigger }: PropsType): ReactElement => {
                     rows={5}
                     maxLength={50}
                     value={textareaInput}
-                    onChange={onTextareaChange}
+                    onChange={(e) => setTextareaInput(e.target.value)}
                 />
+
+                {(isUpdateError || isSaveError) && (
+                    <div className="sm-alert errmsg">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                        <span>{responseMsg}</span>
+                    </div>
+                )}
+
                 <button
                     className="action-btn"
-                    type="button"
-                    onClick={isBuildAuthor ? onUpdateBuildClicked : onSaveBuildClicked}
+                    type="submit"
+                    onClick={onSaveBuildClicked}
                 >
-                    {isBuildAuthor ? "Update" : "Save"}
+                    {(!isSaveLoading && !isUpdateLoading) ? buttonText :
+                        <ClipLoader
+                            color={"rgb(231, 214, 182)"}
+                            loading={isBuildAuthor ? isUpdateLoading : isSaveLoading}
+                            size={20}
+                            aria-label="Loading Spinner"
+                            data-testid="loader"
+                        />
+                    }
                 </button>
-                {(isSaveLoading || isUpdateLoading) &&  <p>is Loading...</p>}
             </div>
         </Alert>
     )
