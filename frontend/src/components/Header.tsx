8eb1e09397a95f444ac018c80381466812185335
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-31 01:21:44 +0200

    Frontend: Update Header
    rearranged links, added classnames and wrappers for styling.

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,68 +1,80 @@
 import { ReactElement } from "react";
 import { Link } from "react-router-dom";
+import { MdLogout } from "react-icons/md";
 import { useSendLogoutMutation } from "../features/auth/authApiSlice";
 import useAuth from "../hooks/useAuth";
 
 const Header = (): ReactElement => {
 
-    const { userId, username, status, isUser, isDemoadmin, isAdmin } = useAuth();
+    const { userId, status, isUser, isDemoadmin, isAdmin } = useAuth();
 
     const [sendLogout, {}] = useSendLogoutMutation();
 
     return (
         <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/builds">
-                            Builds
-                        </Link>
-                    </li>
-                    {(isAdmin || isDemoadmin) && (
-                        <li>
-                            <Link to="/users">
-                                Users
+            <nav className="primary-nav">
+                <div className="page-padding">
+                    <div className="container-large">
+                        <div className="nav-inner">
+                            <Link className="nav-logo" to={"/"}>
+                                <div>ELDENPLANNER</div>
                             </Link>
-                        </li>
-                    )}
-                    <li>
-                        <Link to="/charplanner">
-                            Charplanner
-                        </Link>
-                    </li>
-                    {status === "Visitor" && (
-                        <li>
-                            <Link to="/signup">
-                                Sign Up
-                            </Link>
-                        </li>
-                    )}
-                    {status === "Visitor" && (
-                        <li>
-                            <Link to="/login">
-                                Login
-                            </Link>
-                        </li>
-                    )}
-                    {status !== "Visitor" && (
-                        <li>
-                            <button
-                                type="button"
-                                onClick={sendLogout}
-                                style={{ backgroundColor: "white"}}
-                            >
-                                Logout
-                            </button>
-                        </li>
-                    )}
-                    {(isUser || isDemoadmin || isAdmin) && (
-                        <li>
-                            <Link to={`/${userId}`}>
-                                {username}
-                            </Link>
-                        </li>
-                    )}
-                </ul>
+
+                            <ul>
+                                <li>
+                                    <Link className="nav-link" to="/charplanner">
+                                        Charplanner
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link className="nav-link" to="/builds">
+                                        Community Builds
+                                    </Link>
+                                </li>
+                                {(isAdmin || isDemoadmin) && (
+                                    <li>
+                                        <Link className="nav-link" to="/users">
+                                            Users
+                                        </Link>
+                                    </li>
+                                )}
+                                {status === "Visitor" && (
+                                    <li>
+                                        <Link className="nav-link" to="/login">
+                                            Login
+                                        </Link>
+                                    </li>
+                                )}
+                                {status === "Visitor" && (
+                                    <li>
+                                        <Link className="nav-link" to="/signup">
+                                            Sign Up
+                                        </Link>
+                                    </li>
+                                )}
+                                {(isUser || isDemoadmin || isAdmin) && (
+                                    <li>
+                                        <Link className="nav-link" to={`/${userId}`}>
+                                            Your Profile
+                                        </Link>
+                                    </li>
+                                )}
+                                {status !== "Visitor" && (
+                                    <li>
+                                        <button
+                                            className="nav-link"
+                                            type="button"
+                                            onClick={sendLogout}
+                                        >
+                                            <div>Logout</div>
+                                            <MdLogout />
+                                        </button>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </nav>
         </header>
     )
