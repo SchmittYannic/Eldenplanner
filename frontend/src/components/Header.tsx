40a1c923a80a8e8ca799aa7912ea669fe6527964
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-08-31 03:27:27 +0200

    Frontend: Added mobile-menu

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { Link } from "react-router-dom";
 import { MdLogout, MdMenu } from "react-icons/md";
 import { useSendLogoutMutation } from "../features/auth/authApiSlice";
@@ -9,10 +9,9 @@ import { navbarlogo } from "../assets";
 const Header = (): ReactElement => {
 
     const { userId, status, isUser, isDemoadmin, isAdmin } = useAuth();
-
     const windowSize = useWindowSize();
-
     const [sendLogout, {}] = useSendLogoutMutation();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const isSmallDesktop = windowSize.width && windowSize.width < 1040;
     const isMobile = windowSize.width && windowSize.width < 840;
@@ -38,6 +37,7 @@ const Header = (): ReactElement => {
                                 <button
                                     className="mobile-menu-button"
                                     type="button"
+                                    onClick={() => setIsMenuOpen(!isMenuOpen)}
                                 >                                      
                                     <MdMenu />
                                 </button>
@@ -98,6 +98,69 @@ const Header = (): ReactElement => {
                         </div>
                     </div>
                 </div>
+
+                {(isMenuOpen && isMobile) && (
+                    <div className="mobile-menu">
+                        <ul>
+                            <li>
+                                <Link className="nav-link" to="/charplanner">
+                                    Charplanner
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className="nav-link" to="/builds">
+                                    Community Builds
+                                </Link>
+                            </li>
+                            {(isAdmin || isDemoadmin) && (
+                                <li>
+                                    <Link className="nav-link" to="/users">
+                                        Users
+                                    </Link>
+                                </li>
+                            )}
+                        </ul>
+
+                        <div className="horizontal-divider" />
+
+                        <ul>
+                            {status === "Visitor" && (
+                                <li>
+                                    <Link className="nav-link" to="/login">
+                                        Login
+                                    </Link>
+                                </li>
+                            )}
+                            {status === "Visitor" && (
+                                <li>
+                                    <Link className="nav-link" to="/signup">
+                                        Sign Up
+                                    </Link>
+                                </li>
+                            )}
+                            {(isUser || isDemoadmin || isAdmin) && (
+                                <li>
+                                    <Link className="nav-link" to={`/${userId}`}>
+                                        Your Profile
+                                    </Link>
+                                </li>
+                            )}
+                            {status !== "Visitor" && (
+                                <li>
+                                    <button
+                                        className="nav-link"
+                                        type="button"
+                                        onClick={sendLogout}
+                                    >
+                                        <div>Logout</div>
+                                        <MdLogout />
+                                    </button>
+                                </li>
+                            )}
+                        </ul>
+                    </div>
+                )}
+                
             </nav>
         </header>
     )
