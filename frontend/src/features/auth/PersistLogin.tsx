24e5d8f0efb8cb910d0b042e22a885a943cd7c3a
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-09-02 17:32:38 +0200

    Frontend: Removing console.logs and adding spinner to PersistLogin

--- frontend/src/features/auth/PersistLogin.tsx
+++ frontend/src/features/auth/PersistLogin.tsx
@@ -1,5 +1,6 @@
-import { Outlet, Link } from "react-router-dom"
-import { useEffect, useRef, useState } from "react"
+import { Outlet } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { ClipLoader } from "react-spinners";
 import { useRefreshMutation } from "./authApiSlice"
 import usePersist from "../../hooks/usePersist"
 import { useSelector } from "react-redux"
@@ -7,36 +8,33 @@ import { selectCurrentToken } from "./authSlice"
 
 const PersistLogin = () => {
 
-    const [persist] = usePersist()
-    const token = useSelector(selectCurrentToken)
-    const effectRan = useRef(false)
+    const [persist] = usePersist();
+    const token = useSelector(selectCurrentToken);
+    const effectRan = useRef(false);
 
-    const [trueSuccess, setTrueSuccess] = useState(false)
+    const [trueSuccess, setTrueSuccess] = useState(false);
 
     const [refresh, {
         isUninitialized,
         isLoading,
         isSuccess,
         isError,
-        error
     }] = useRefreshMutation();
-    console.log(isUninitialized, isLoading, isSuccess, isError)
-
 
     useEffect(() => {
 
         if (effectRan.current === true || process.env.NODE_ENV !== "development") { // React 18 Strict Mode
 
             const verifyRefreshToken = async () => {
-                console.log("verifying refresh token")
+                //console.log("verifying refresh token")
                 try {
                     //const response = 
-                    await refresh("")
+                    await refresh("").unwrap();
                     //const { accessToken } = response.data
                     setTrueSuccess(true);
                 }
                 catch (err) {
-                    console.error(err);
+                    
                 }
             }
 
@@ -51,27 +49,38 @@ const PersistLogin = () => {
 
     let content
     if (!persist) { // persist: no
-        console.log("no persist")
+        //console.log("no persist")
         content = <Outlet />
     } else if (isLoading) { //persist: yes, token: no
-        console.log("loading")
-        content = <p>...is Loading</p>
+        //console.log("loading")
+        content = (
+            <main className="page-spinner">
+                <ClipLoader
+                    color={"rgb(231, 214, 182)"}
+                    loading={isLoading}
+                    size={30}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                />
+            </main>
+        )
     } else if (isError) { //persist: yes, token: no
         // console.log("error")
         // content = (
-        //     <p className="errmsg" style={{ marginTop: "50px"}}>
-        //         {`${isCustomError(error) ? error?.data?.message : "An Error occured"} - `}
-        //         <Link to="/login">Please login again</Link>.
-        //     </p>
+        //     <main>
+        //         <div className="sm-alert errmsg">
+        //             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+        //             <span>Something went wrong. <Link to="/login">Please login again</Link></span>
+        //         </div>
+        //     </main>
         // )
-        console.log("not logged in")
+        //console.log("not logged in")
         content = <Outlet />
     } else if (isSuccess && trueSuccess) { //persist: yes, token: yes
-        console.log("success")
+        //console.log("success")
         content = <Outlet />
     } else if (token && isUninitialized) { //persist: yes, token: yes
-        console.log("token and uninit")
-        console.log(isUninitialized)
+        //console.log("token and uninit")
         content = <Outlet />
     }
 
