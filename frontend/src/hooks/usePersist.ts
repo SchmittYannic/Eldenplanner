8a67c941fc9885f1ae005c4b8ce307837f69a0ca
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-09-02 17:31:42 +0200

    Frontend: Always enable persist by setting it to true by default

--- frontend/src/hooks/usePersist.ts
+++ frontend/src/hooks/usePersist.ts
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react"
 const usePersist = (): [boolean, React.Dispatch<React.SetStateAction<boolean>>] => {
     const persistInLocalStorage: string | null = localStorage.getItem("persist");
 
-    const [persist, setPersist] = useState<boolean>(typeof persistInLocalStorage === "string" ? JSON.parse(persistInLocalStorage) : false);
+    const [persist, setPersist] = useState<boolean>(typeof persistInLocalStorage === "string" ? JSON.parse(persistInLocalStorage) : true);
 
     useEffect(() => {
         localStorage.setItem("persist", JSON.stringify(persist));
