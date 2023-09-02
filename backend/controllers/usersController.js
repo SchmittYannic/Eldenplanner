41168ed35b9948ceb9edcb9ad7ad406d9441971c
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-09-02 19:35:38 +0200

    Backend: Move validation of received user data from createNewUser to checkNewUserData
    Move validation of received user data from createNewUser in usersController to middleware checkNewUserData.
    
    This way a middleware can be executed after the validation of received user data and before the new user gets added to the database. This middleware is going to be a rateLimiter, which only allows 1 request per day. So a user can only add 1 new account per day.
    
    This enables a user to send multiple request containing invalid user data. For example when a username or email is already in use, the user can send follow up requests. But once a user has send valid user data to create a new account the rateLimit will make sure he cant create more accounts after that.

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -20,50 +20,13 @@ const getAllUsers = async (req, res) => {
 // @route POST /users
 // @access Public
 const createNewUser = async (req, res) => {
-    const { username, password, email } = req.body;
-
-    /* Confirm data */
-    if (!username || !password || !email) {
-        return res.status(400).json({ message: "All fields are required" });
-    }
-
-    const validUsernameRegex = /^[A-Za-z][A-Za-z0-9_]{7,19}$/;
-    const isValidUsername = validUsernameRegex.test(username);
-
-    if (!isValidUsername) {
-        return res.status(400).json({ message: "Invalid username received" });
-    }
-
-    /* Check for duplicate */
-    // if you use async await and expect a promise back u should use exec at the end.
-    // collation to make sure it is case insensitive -> Hank and hank count as duplicates
-    const duplicateUsername = await User.findOne({ username }).collation({ locale: 'en', strength: 2 }).lean().exec();
-
-    if (duplicateUsername) {
-        return res.status(409).json({ message: "Username already in use" });
-    }
-
-    const duplicateEmail = await User.findOne({ email }).lean().exec();
-
-    if (duplicateEmail) {
-        return res.status(409).json({ message: "Email already in use" });
-    }
-
-    if (!EmailValidator.validate(email)) {
-        return res.status(400).json({ message: "Invalid email address received" });
-    }
-
-    /* Hash password */
-    const hashedPwd = await bcrypt.hash(password, 10); // salt rounds
-
-    const userObject = { username, "password": hashedPwd, email };
-
+    const userObject = req.userData;
     /* Create and store new user in DB */
     const user = await User.create(userObject);
 
     if (user) {
         // created user successfully
-        res.status(201).json({ message: `New user ${username} created` });
+        res.status(201).json({ message: `New user ${userObject.username} created` });
     } else {
         res.status(400).json({ message: "Invalid user data received" });
     }
