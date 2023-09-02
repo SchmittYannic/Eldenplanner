2236952accb95e8a34950814b97c5fde38fe478d
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-09-02 10:30:37 +0200

    Frontend: Updated UserBuilds and UserPage with ClipLoader and error messages

--- frontend/src/features/users/UserBuilds.tsx
+++ frontend/src/features/users/UserBuilds.tsx
@@ -1,36 +1,33 @@
 import { ReactElement } from "react"
-import { useSelector } from "react-redux";
-import { BuildType, selectAllBuilds } from "../builds/buildsApiSlice";
-import { UserType, useGetUsersQuery } from "./usersApiSlice";
+import { ClipLoader } from "react-spinners";
+import { BuildType, useGetBuildsQuery } from "../builds/buildsApiSlice";
+import { UserType } from "./usersApiSlice";
 import { calcSumObjectValues } from "../../utils/functions";
 import UserBuildsList from "./UserBuildsList";
+import { isCustomError } from "../../app/api/apiSlice";
 
 type PropsType = {
-    userId: string,
+    author: UserType,
 }
 
-const UserBuilds = ({ userId }: PropsType): ReactElement => {
+const UserBuilds = ({ author }: PropsType): ReactElement => {
 
-    const allBuilds = useSelector(selectAllBuilds) as BuildType[];
+    const {
+        data: buildsAsEntityState,
+        isSuccess,
+        isLoading,
+        isError,
+        error,
+    } = useGetBuildsQuery("buildsList");
+
+    const allBuilds = buildsAsEntityState && Object.values(buildsAsEntityState.entities) as BuildType[];
     
-    const { author } = useGetUsersQuery("usersList", {
-        selectFromResult: ({ data }) => ({
-            author: data?.entities[userId] as UserType
-        }),
-    });
-    
+    const ownBuilds = allBuilds && allBuilds.filter((build) => build.user === author.id.toString());
 
-    const builds = allBuilds.filter((build: BuildType) => build.user === userId);
-
-    const tableData = allBuilds.length > 0 && author && builds.map((build) => {
+    const tableData = ownBuilds && ownBuilds.map((build) => {
 
         const sumStats = calcSumObjectValues(build.stats);
-
         const runelevel = sumStats - 79;
-
-        if(!author) {
-            throw new Error("Builds tableData not complete");
-        }
 
         return { 
             buildId: build.id,
@@ -44,19 +41,36 @@ const UserBuilds = ({ userId }: PropsType): ReactElement => {
         }
     });
 
-    if (tableData) {
+    if (isSuccess && tableData) {
         return (
             <>
                 <UserBuildsList data={tableData} />
             </>
         )
-    } else {
+    } else if (isLoading) {
         return (
-            <main>
-                <p>TESTING</p>
-            </main>
+            <ClipLoader
+                color={"rgb(231, 214, 182)"}
+                loading={isLoading}
+                size={20}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+            />
         )
-    }
+    } else if (isError) {
+        const errormsg = isCustomError(error) && (error.status === 400 || error.status === 401) && error.data.message;
+        return (
+            <div className="sm-alert errmsg">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                <span>{errormsg ? errormsg : "an error occured"}</span>
+            </div>
+        )
+    } return (
+        <div className="sm-alert errmsg">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <span>Something went wrong.</span>
+        </div>
+    )
 }
 
 export default UserBuilds
--- frontend/src/features/users/UserPage.tsx
+++ frontend/src/features/users/UserPage.tsx
@@ -1,10 +1,10 @@
 import { ReactElement } from "react";
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { UserType, selectUserById } from "./usersApiSlice";
-import { RootState } from "../../app/store";
+import { ClipLoader } from "react-spinners";
+import { UserType, useGetUsersQuery } from "./usersApiSlice";
 import useAuth from "../../hooks/useAuth";
 import UserBuilds from "./UserBuilds";
+import { isCustomError } from "../../app/api/apiSlice";
 
 const UserPage = (): ReactElement => {
 
@@ -12,19 +12,20 @@ const UserPage = (): ReactElement => {
     const userId = param?.userId;
     const { username } = useAuth();
 
-    const user = useSelector((state: RootState) => {
-        if(userId) {
-            return selectUserById(state, userId) as UserType
-        }
-        return null
-    });
+    const { 
+        data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error,
+    } = useGetUsersQuery("usersList");
 
+    const user = isSuccess && userId && users.entities[userId] as UserType;
     const userSince = user && new Date(user.createdAt);
-
     const month = userSince && userSince.toLocaleString("default", { month: "long" });
     const year = userSince && userSince.toLocaleString("default", { year: "numeric" });
 
-    const isOwnProfile = username === user?.username;
+    const isOwnProfile = user ? username === user?.username : false;
 
     const onEditProfileClicked = () => {
 
@@ -54,9 +55,31 @@ const UserPage = (): ReactElement => {
                     <div className="divider-4" />
     
                     <div className="userpage__userbuilds">
-                        {userId && <UserBuilds userId={userId} />}
+                        {userId && <UserBuilds author={user} />}
                     </div>
                 </section>
+            </main>
+        )
+    } else if (isLoading) {
+        return (
+            <main>
+                <ClipLoader
+                    color={"rgb(231, 214, 182)"}
+                    loading={isLoading}
+                    size={20}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                />
+            </main>
+        )
+    } else if (isError) {
+        const errormsg = isCustomError(error) && (error.status === 400 || error.status === 401) && error.data.message;
+        return (
+            <main>
+                <div className="sm-alert errmsg">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                    <span>{errormsg ? errormsg : "an error occured"}</span>
+                </div>
             </main>
         )
     } else {
