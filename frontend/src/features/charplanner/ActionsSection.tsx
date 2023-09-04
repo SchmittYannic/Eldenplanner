338bcc609202f4b5eb77f6e1f518ceedaa1dd9b0
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-09-04 23:23:03 +0200

    Frontend: Add delete button to ActionsSection

--- frontend/src/features/charplanner/ActionsSection.tsx
+++ frontend/src/features/charplanner/ActionsSection.tsx
@@ -1,6 +1,7 @@
 import { ReactElement, useState } from "react";
 import SaveBuild from "./SaveBuild";
 import useAuth from "../../hooks/useAuth";
+import DeleteBuild from "./DeleteBuild";
 
 type PropsType = {
     isBuildAuthor: boolean
@@ -10,18 +11,33 @@ const ActionsSection = ({ isBuildAuthor }: PropsType): ReactElement => {
 
     const { status } = useAuth();
 
+    const [isDeleteTriggered, setIsDeleteTriggered] = useState(false);
     const [isSaveTriggered, setIsSaveTriggered] = useState(false);
 
     const onSaveClicked = () => {
         if (status !== "Visitor") {
             setIsSaveTriggered(true);
         }
-        // else missing for when Visitor presses save
-        // show some errmsg
     };
 
+    const onDeleteClick = () => setIsDeleteTriggered(true);
+
     return (
-        <section className="Charplanner__actionscontainer">     
+        <section className="Charplanner__actionscontainer">
+            {isBuildAuthor && (
+                <button
+                    className="charplanner__delete button"
+                    type="button"
+                    onClick={onDeleteClick}
+                    title="Delete Build"
+                >
+                    Delete
+                </button>
+            )}
+            {isDeleteTriggered && 
+                <DeleteBuild setTrigger={setIsDeleteTriggered} />
+            }
+
             <button
                 className="Charplanner__Save action-btn"
                 type="button"
