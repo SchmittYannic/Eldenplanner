530327a4fdb92887dc49b7aa84997f6656294e82
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-09-22 19:09:54 +0200

    Frontend: rename CustomSelect2 to CustomSelect

--- frontend/src/components/ui/CustomSelect.tsx
+++ frontend/src/components/ui/CustomSelect.tsx
@@ -1,69 +1,207 @@
-import { useState, useEffect, ChangeEvent, ReactElement } from "react";
-import CustomSelectLogic from "./CustomSelectLogic";
+import { ReactElement, useState, useRef, useEffect, ChangeEvent, KeyboardEvent } from "react";
+import { MdClose, MdExpandMore } from "react-icons/md";
 import "./CustomSelect.scss";
+import "./ListElement.scss";
 
-type PropsType = {
+type SearchSelectPropsType = {
     id: string,
-    selectedOption: string,
-    setSelectedOption: React.Dispatch<React.SetStateAction<string>> | Function,
+    value: string,
+    setValue: Function,
     options: string[],
-    renderOption: Function,
-    classes: string,
-    label: string,
+    className?: string,
+    label?: string,
     enableDelete?: boolean,
     searchable?: boolean,
-    disabled?: boolean,
-}
+    disabled?: boolean
+};
 
-const CustomSelect = ({ 
+const CustomSelect = ({
     id,
-    selectedOption, 
-    setSelectedOption, 
-    options, 
-    renderOption, 
-    classes="", 
-    label="", 
-    enableDelete=false, 
+    value: currentSelectedOption,
+    setValue,
+    options,
+    className="CustomSelect",
+    label="",
+    enableDelete=false,
     searchable=false,
     disabled=false,
-}: PropsType): ReactElement => {
-    const [filteredOptions, setFilteredOptions] = useState(options);
+}: SearchSelectPropsType): ReactElement => {
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const { target } = e;
-        const filteredOptions = options.filter((option) => option
-        .toLowerCase().includes(target.value.toLowerCase()));
+    const [inputValue, setInputValue] = useState<string>(currentSelectedOption);
+    const [filteredOptions, setFilteredOptions] = useState<string []>(options);
+    const [showOptions, setShowOptions] = useState<boolean>(false);
 
-        setFilteredOptions(filteredOptions);
+    const [focusedOption, setFocusedOption] = useState(-1);
+    const optionContainer = useRef<HTMLLIElement | null>(null);
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const currentInputValue = e.target.value;
+        setInputValue(currentInputValue);
+
+        const newFilteredOptions = options.filter((option) => option
+        .toLowerCase().includes(currentInputValue.toLowerCase()));
+        setFilteredOptions(newFilteredOptions);
+
+        setShowOptions(true);
+        setFocusedOption(-1); // set focusedOption back to -1 when input changed
     };
 
-    const handleSelect = (option: string) => {
-        setSelectedOption(option);
-        setFilteredOptions(options);
+    const handleSelection = (idx: number) => {
+        setInputValue(filteredOptions[idx]);
     }
+
+    const handleClose = () => {
+        setShowOptions(false);
+        setFocusedOption(-1);
+        setFilteredOptions(options);
+    };
+
+    const handleReset = () => {
+        setInputValue("");
+        setValue("");
+    };
+
+    const handleBlur = () => {
+        /* if inputValue is an option set the selected value of the select box */
+        if (options.includes(inputValue)) {
+            setValue(inputValue);
+        } else if (!options.includes(inputValue) && !enableDelete) {
+            /* if inputValue is not an option AND you are not allowed to select nothing 
+            reset the select box to its previous value */
+            setInputValue(currentSelectedOption);
+            setValue(currentSelectedOption);
+        } else if (!options.includes(inputValue) && enableDelete && inputValue && currentSelectedOption.startsWith(inputValue)) {
+            /* if inputValue is not an option AND 
+            you are allowed to select nothing AND
+            the inputValue is not an empty string AND
+            the currentSelectedOption starts with the inputValuu THEN
+            reset the select box to its previous value */
+            setInputValue(currentSelectedOption);
+            setValue(currentSelectedOption);
+        } else {
+            /* else select nothing */
+            handleReset();
+        }
+
+        setTimeout(()=> handleClose(), 100); //Timeout for ripple effect to play, when Option is clicked.
+    };
+
+    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+        const { key } = e;
+        const optionsLength = options.length;
+        let nextIndexCount = 0;
+
+        if (key === "ArrowDown") {
+            nextIndexCount = (focusedOption + 1) % optionsLength;
+            setFocusedOption(nextIndexCount);
+        }
+
+        if (key === "ArrowUp") {
+            nextIndexCount = (focusedOption + optionsLength - 1) % optionsLength;
+            setFocusedOption(nextIndexCount);
+        }
+
+        if (key === "Enter") {
+            if (showOptions) {
+                if (focusedOption !== -1) {
+                    setInputValue(filteredOptions[focusedOption]);
+                    setValue(filteredOptions[focusedOption]);
+                    handleClose();
+                } else {
+                    handleBlur();
+                }
+            } else {
+                setShowOptions(true);
+            }
+        }
+
+        if (key === "Tab") {
+            if (showOptions) {
+                e.preventDefault();
+                if (focusedOption !== -1) {
+                    setInputValue(filteredOptions[focusedOption]);
+                    handleClose();
+                } else {
+                    handleBlur();
+                }
+            }
+        }
+    };
+
+    const handleClickExpandButton = () => {
+        if (!showOptions) {
+            const inputElement = document.getElementById(id);
+            inputElement?.focus();
+            setShowOptions(true);
+        }
+    };
+
+    const handleButtonKeyDown = (e: KeyboardEvent<HTMLButtonElement>) => {
+        const { key } = e;
+        if (key === "Enter") inputValue==="" ? handleClickExpandButton() : handleReset();
+    };
 
     useEffect(() => {
         setFilteredOptions(options);
     }, [options]);
 
+    useEffect(() => {
+        setInputValue(currentSelectedOption);
+    }, [currentSelectedOption]);
+
+    useEffect(() => {
+        if(!optionContainer.current) return;
+
+        optionContainer.current.scrollIntoView({
+            block: "center",
+        });
+    }, [focusedOption]);
+
     return (
-        <form 
-            className={classes === "" ? "CustomSelect" : classes}
-            onSubmit={(e) => e.preventDefault()}
-        >
-            <CustomSelectLogic 
-                id={id}
-                currentSelectedOption={selectedOption}
-                options={filteredOptions}
-                renderOption={renderOption}
-                label={label}
-                enableDelete={enableDelete}
-                searchable={searchable}
-                disabled={disabled}
-                onChange={handleChange}
-                onSelect={(option: string) => handleSelect(option)}
-            />
-        </form>
+        <div className={className}>
+            <label>
+                <input
+                    id={id}
+                    type="text"
+                    placeholder=" "
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onKeyDown={handleKeyDown}
+                    onClick={() => setShowOptions(true)}
+                    onBlur={handleBlur}
+                    readOnly={!searchable}
+                    disabled={disabled}
+                />
+                <p>{label}</p>
+                <button tabIndex={disabled ? -1 : 0} onKeyDown={handleButtonKeyDown} >
+                    {enableDelete ? 
+                        inputValue === "" ? 
+                        <MdExpandMore  className="ddBtn" onClick={handleClickExpandButton} /> : 
+                        <MdClose className="ddBtn" onMouseDown={handleReset} />
+                    : <MdExpandMore  className="ddBtn" onClick={handleClickExpandButton} /> 
+                    }
+                </button>
+            </label>
+
+            {showOptions && 
+                <ul className="OptionsList">
+                    {filteredOptions.map((option, idx) => {
+                        return(
+                            <li 
+                                key={idx}
+                                ref={idx === focusedOption ? optionContainer : null}
+                                className={idx === focusedOption ? "FocusedOption": ""}
+                                onMouseDown={() => handleSelection(idx)}
+                            >
+                                <p className={option === currentSelectedOption ? "SelectedOption" : ""}>
+                                    {option}
+                                </p>
+                            </li>
+                        )
+                    })}
+                </ul>
+            }
+        </div>
     )
 }
 
--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,7 +1,6 @@
 import Checkbox from "./Checkbox";
 import TextInput from "./TextInput";
 import CustomSelect from "./CustomSelect";
-import CustomSelect2 from "./CustomSelect2";
 import Alert from "./Alert";
 import CollapsibleList from "./CollapsibleList";
 import DebouncedInput from "./DebouncedInput";
@@ -11,7 +10,6 @@ export {
     Checkbox,
     TextInput,
     CustomSelect,
-    CustomSelect2,
     Alert,
     CollapsibleList,
     DebouncedInput,
