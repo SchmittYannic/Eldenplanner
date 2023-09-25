db0ee4231734d6a6523148a0c72a51d9408a6039
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-09-25 23:50:19 +0200

    Frontend: Install pluginRewriteAll for vite
    Used to make dots in the url work so jsonwebtoken can be used inside the url as params for email verification.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -8,6 +9,9 @@ export default defineConfig(({ command, mode }) => {
     define: {
       'process.env.API_BASEURL': JSON.stringify(env.API_BASEURL)
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      pluginRewriteAll(),
+    ],
   };
 });
