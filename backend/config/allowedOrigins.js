3200772e6cf28decfe359c43175da0f6a2cbe48a
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-09-26 02:18:02 +0200

    Backend: Updated allowedOrigins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:5173'
+    "http://localhost:5173",
+    "https://eldenplanner.onrender.com/"
 ];
 
 export default allowedOrigins;
