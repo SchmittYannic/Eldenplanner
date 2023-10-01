17ca3f1dcb28f5a2f41bc1db8da1777aea0269bd
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-10-01 04:01:53 +0200

    Frontend: Added Toast Component to Layout

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,12 +1,14 @@
 import { ReactElement } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Toast from "./Toast";
 
 const Layout = (): ReactElement => {
     return (
         <>
             <Header />
             <Outlet />
+            <Toast />
         </>
     )
 };
