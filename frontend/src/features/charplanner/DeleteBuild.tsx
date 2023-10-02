9c8eb48292d50a59625e9b3a478c9234a22230af
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-10-02 23:39:55 +0200

    Frontend: Using FormInput in DeleteBuild

--- frontend/src/features/charplanner/DeleteBuild.tsx
+++ frontend/src/features/charplanner/DeleteBuild.tsx
@@ -12,7 +12,8 @@ import {
     DialogButtons,
     DialogContent,
     DialogIcon,
-    DialogMain
+    DialogMain,
+    FormInput,
 } from "../../components/ui";
 
 type PropsType = {
@@ -42,7 +43,7 @@ const DeleteBuild = ({ setTrigger }: PropsType): ReactElement => {
         } catch (err: any) {
             if (!err.status) {
                 setResponseMsg("No Server Response");
-            } else if (err.status === 400 || err.status === 401) {
+            } else if ([400, 401].includes(err.status)) {
                 setResponseMsg(err.data?.message);
             } else {
                 setResponseMsg("an error occured");
@@ -54,72 +55,70 @@ const DeleteBuild = ({ setTrigger }: PropsType): ReactElement => {
 
     return (
         <Dialog className="dialog__deletebuild" setDialog={setTrigger}>
-            <DialogMain>
-                <DialogIcon>
-                    <MdWarningAmber />
-                </DialogIcon>
-                <DialogContent>
-                    <h3>Confirm Build Deletion</h3>
+            <form action="" onSubmit={(e) => e.preventDefault()}>
+                <DialogMain>
+                    <DialogIcon>
+                        <MdWarningAmber />
+                    </DialogIcon>
+                    <DialogContent>
+                        <h3>Confirm Build Deletion</h3>
 
-                    <div className="divider-4" />
-    
-                    <p>
-                        Are you sure you want to delete this build?
-                        Deleted builds are unrecoverable and lost forever.
-                    </p> 
+                        <div className="divider-4" />
+        
+                        <p>
+                            Are you sure you want to delete this build?
+                            Deleted builds are unrecoverable and lost forever.
+                        </p> 
 
-                    <div className="divider-4" />
+                        <div className="divider-4" />
 
-                    <div className="input-wrapper">
-                        <label htmlFor="confirmdeletion">
-                            To Confirm, type DELETE in the field below
-                        </label>
-                        <div className="divider-1" />
-                        <input
+                        <FormInput
                             id="confirmdeletion"
                             name="confirmdeletion"
                             type="text"
-                            maxLength={30}
+                            maxLength={20}
                             value={inputValue}
                             onChange={(e) => setInputValue(e.target.value)}
                             autoComplete="off"
-                        />
-                    </div>
+                        >
+                            To Confirm, type DELETE in the field below
+                        </FormInput>
 
-                    <div className="divider-4" />
+                        <div className="divider-4" />
 
-                    {isError ? (
-                        <>
-                            <div className="divider-4" />
-                            <div className="sm-alert errmsg full">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                <span>{responseMsg}</span>
-                            </div>
-                        </>
-                    ) : (<></>)}
-                </DialogContent>
-            </DialogMain>
-            <DialogButtons>
-                <button
-                    className="button"
-                    type="button"
-                    onClick={() => setTrigger(false)}
-                    title="Cancel Deletion"
-                >
-                    Cancel
-                </button>
+                        {isError ? (
+                            <>
+                                <div className="divider-4" />
+                                <div className="sm-alert errmsg full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                    <span>{responseMsg}</span>
+                                </div>
+                            </>
+                        ) : (<></>)}
+                    </DialogContent>
+                </DialogMain>
+                <DialogButtons>
+                    <button
+                        className="button"
+                        type="button"
+                        onClick={() => setTrigger(false)}
+                        title="Cancel Deletion"
+                    >
+                        Cancel
+                    </button>
 
-                <AsyncButton
-                    isLoading={isLoading}
-                    className="action-btn"
-                    type="submit"
-                    onClick={onConfirmDeletionClicked}
-                    disabled={!isDELETE}
-                    title={!isDELETE ? "type DELETE into the field above" : "Confirm Deletion"}
-                >
-                    Delete
-                </AsyncButton>
-            </DialogButtons>
+                    <AsyncButton
+                        isLoading={isLoading}
+                        className="action-btn"
+                        type="submit"
+                        onClick={onConfirmDeletionClicked}
+                        disabled={!isDELETE}
+                        title={!isDELETE ? "type DELETE into the field above" : "Confirm Deletion"}
+                    >
+                        Delete
+                    </AsyncButton>
+                </DialogButtons>
+            </form>
         </Dialog>
     )
 }
