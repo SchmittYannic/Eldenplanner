888738ea6eab2f260b2a6e06baf03762b6b559f3
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2023-10-06 08:03:16 +0200

    Frontend: moved herosection of Frontpage into own component

---
+++ frontend/src/components/Herosection.tsx
@@ -0,0 +1,38 @@
+import { ReactElement } from "react";
+import { Link } from "react-router-dom";
+
+import useWindowSize from "../hooks/useWindowSize";
+import ParticlesHeroBg from "./ParticlesHeroBg";
+import { herobg } from "../assets";
+
+const Herosection = (): ReactElement => {
+
+    const windowSize = useWindowSize();
+    const isMobile = windowSize.width && windowSize.width < 850;
+
+    return (
+        <section className="hero">
+            <img className="hero-img" src={herobg} alt="elden-planner-logo-outline" />
+            {!isMobile && <ParticlesHeroBg />}
+            <div className="hero-foreground">
+                <div className="hero-text">
+                    <h1 className="gold-text-background">ELDENPLANNER</h1>
+                    <div className="divider-4" />
+                    <div className="divider-4" />
+                    <div className="divider-4" />
+                    <p>
+                        A Character Build Planner for the game <a href="https://de.bandainamcoent.eu/elden-ring/elden-ring" target="_blank">Elden Ring</a>. The perfect starting point for your next adventure through the Lands Between.
+                    </p>
+                    <div className="divider-4" />
+                    <div className="divider-4" />
+                    <div className="divider-4" />
+                    <Link to={"/charplanner"} className="action-btn">
+                        Get started
+                    </Link>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Herosection
