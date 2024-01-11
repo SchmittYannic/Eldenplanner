bb3f4cfb284c189493472b2b39cde80aa8af4bcd
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-01-11 19:23:19 +0100

    Frontend: Adds ContentDialog to ContactSection

--- frontend/src/components/ContactSection.tsx
+++ frontend/src/components/ContactSection.tsx
@@ -1,5 +1,11 @@
+import { Link, useParams } from "react-router-dom";
+
+import ContactDialog from "./ContactDialog";
 
 const ContactSection = () => {
+
+    const params = useParams();
+
     return (
         <section className="contact-section full">
             <div className="horizontal-divider full" />
@@ -9,15 +15,14 @@ const ContactSection = () => {
             </h2>
             <div className="contact-section-content">
                 <p>
-                    Contact us here and tell us your ideas of how to improve Eldenplanner even more.
+                    Contact us here and give us feedback about how we can improve Eldenplanner even more.
                 </p>
 
-                <button
-                    type="button"
-                    className="action-btn"
-                >
+                <Link to={"/contactform"} className="action-btn">
                     Contact
-                </button>
+                </Link>
+
+                {params?.params === "contactform" && <ContactDialog />}
             </div>
         </section>
     )
