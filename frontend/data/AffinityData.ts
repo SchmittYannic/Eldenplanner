ddbb5ef4c9bb3dc47a32d0444c8a935cd7850d2c
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-06-25 22:43:18 +0200

    Adds new AffinityData

---
+++ frontend/data/AffinityData.ts
@@ -0,0 +1,21 @@
+export type AffinityDataType = {
+    [key: string]: number
+}
+
+export const AffinityData: AffinityDataType = {
+    "Standard": 0,
+    "Heavy": 100,
+    "Keen": 200,
+    "Quality": 300,
+    "Fire": 400,
+    "Flame Art": 500,
+    "Lightning": 600,
+    "Sacred": 700,
+    "Magic": 800,
+    "Cold": 900,
+    "Poison": 1000,
+    "Blood": 1100,
+    "Occult": 1200,
+}
+
+export const AffinityOptions = Object.keys(AffinityData);
