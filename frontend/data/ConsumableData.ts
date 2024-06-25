33acf45cdc085b7035b387e745e4b21470fea184
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-06-26 01:35:20 +0200

    Adds new ConsumableData

---
+++ frontend/data/ConsumableData.ts
@@ -0,0 +1,487 @@
+type Data = {
+    "AtkID": number,
+    "attackBasePhysics": number,
+    "attackBaseMagic": number,
+    "attackBaseFire": number,
+    "attackBaseThunder": number,
+    "attackBaseDark": number
+}
+
+type ConsumableDataType = {
+    [key: string]: Data
+}
+
+export const ConsumableData: ConsumableDataType = {
+    "Fire Pot": {
+        "AtkID": 10030001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 230,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Oil Pot": {
+        "AtkID": 10038001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Redmane Fire Pot": {
+        "AtkID": 10030101,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 326,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Giantsflame Fire Pot": {
+        "AtkID": 10030201,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 344,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Magic Pot": {
+        "AtkID": 10066001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 322,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Academy Magic Pot": {
+        "AtkID": 10066101,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 405,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Lightning Pot": {
+        "AtkID": 10032000,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 260,
+        "attackBaseDark": 0
+    },
+    "Ancient Dragonbolt Pot": {
+        "AtkID": 10032100,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 340,
+        "attackBaseDark": 0
+    },
+    "Holy Water Pot": {
+        "AtkID": 10035001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 167
+    },
+    "Sacred Order Pot": {
+        "AtkID": 10035101,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 312
+    },
+    "Poison Pot": {
+        "AtkID": 10037001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Fetid Pot": {
+        "AtkID": 10033001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Rot Pot": {
+        "AtkID": 10067001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Swarm Pot": {
+        "AtkID": 10034001,
+        "attackBasePhysics": 116,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Freezing Pot": {
+        "AtkID": 10036001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 119,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Sleep Pot": {
+        "AtkID": 10064000,
+        "attackBasePhysics": 1,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Volcano Pot": {
+        "AtkID": 10060001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 82,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Rancor Pot": {
+        "AtkID": 10065001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 139,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Cursed-Blood Pot": {
+        "AtkID": 10063001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Alluring Pot": {
+        "AtkID": 10039001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Beastlure Pot": {
+        "AtkID": 10039100,
+        "attackBasePhysics": 1,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Albinauric Pot": {
+        "AtkID": 10061001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Roped Fire Pot": {
+        "AtkID": 10040001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 230,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Roped Oil Pot": {
+        "AtkID": 10045001,
+        "attackBasePhysics": 1,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Roped Magic Pot": {
+        "AtkID": 10046001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 322,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Roped Lightning Pot": {
+        "AtkID": 10042000,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 260,
+        "attackBaseDark": 0
+    },
+    "Roped Holy Water Pot": {
+        "AtkID": 10051001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 150
+    },
+    "Roped Poison Pot": {
+        "AtkID": 10044001,
+        "attackBasePhysics": 1,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Roped Fetid Pot": {
+        "AtkID": 10043001,
+        "attackBasePhysics": 1,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Roped Fly Pot": {
+        "AtkID": 10047001,
+        "attackBasePhysics": 116,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Roped Volcano Pot": {
+        "AtkID": 10049001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 82,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Spark Aromatic": {
+        "AtkID": 10351000,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 352,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Uplifting Aromatic": {
+        "AtkID": 10350000,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Poison Spraymist": {
+        "AtkID": 10358000,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Acid Spraymist": {
+        "AtkID": 10361000,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Bloodboil Aromatic": {
+        "AtkID": 10355000,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Ironjar Aromatic": {
+        "AtkID": 10352000,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Bone Dart": {
+        "AtkID": 10171000,
+        "attackBasePhysics": 57,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Poisonbone Dart": {
+        "AtkID": 10172000,
+        "attackBasePhysics": 50,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Crystal Dart": {
+        "AtkID": 10174000,
+        "attackBasePhysics": 66,
+        "attackBaseMagic": 44,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Throwing Dagger": {
+        "AtkID": 10170000,
+        "attackBasePhysics": 67,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Kukri": {
+        "AtkID": 10173000,
+        "attackBasePhysics": 87,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Fan Daggers": {
+        "AtkID": 10175000,
+        "attackBasePhysics": 37,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Ruin Fragment": {
+        "AtkID": 10176000,
+        "attackBasePhysics": 1,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Poisoned Stone": {
+        "AtkID": 10184000,
+        "attackBasePhysics": 50,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Poisoned Stone Clump": {
+        "AtkID": 10184100,
+        "attackBasePhysics": 32,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Explosive Stone": {
+        "AtkID": 10183000,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 108,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Explosive Stone Clump": {
+        "AtkID": 10183100,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 54,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Glintstone Scrap": {
+        "AtkID": 10305000,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 82,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Large Glintstone Scrap": {
+        "AtkID": 10305100,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 65,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Cuckoo Glintstone": {
+        "AtkID": 10303002,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 92,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Gravity Stone Fan": {
+        "AtkID": 10306001,
+        "attackBasePhysics": 160,
+        "attackBaseMagic": 50,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Gravity Stone Chunk": {
+        "AtkID": 10307001,
+        "attackBasePhysics": 247,
+        "attackBaseMagic": 50,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Wraith Calling Bell": {
+        "AtkID": 10308000,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 120,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Ancestral Infant's Head": {
+        "AtkID": 10300001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 55,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 0
+    },
+    "Omen Bairn1": {
+        "AtkID": 10301001,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 105
+    },
+    "Omen Bairn2": {
+        "AtkID": 10301002,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 70
+    },
+    "Regal Omen Bairn1": {
+        "AtkID": 10301101,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 115
+    },
+    "Regal Omen Bairn2": {
+        "AtkID": 10301102,
+        "attackBasePhysics": 0,
+        "attackBaseMagic": 0,
+        "attackBaseFire": 0,
+        "attackBaseThunder": 0,
+        "attackBaseDark": 60
+    }
+}
