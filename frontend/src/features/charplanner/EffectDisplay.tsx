a972d62aa01d00b2cea982df17a10b41ca46df36
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-06-25 20:26:53 +0200

    Fixes EffectDisplay to use the correct data

--- frontend/src/features/charplanner/EffectDisplay.tsx
+++ frontend/src/features/charplanner/EffectDisplay.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 import { useSelector } from "react-redux";
-import { selectGreatrune, selectGreatruneactive, selectTalisman } from "./charplannerSlice";
-import { TalismansData } from "../../../data/TalismansData";
+import { selectArmor, selectGreatrune, selectGreatruneactive, selectTalisman } from "./charplannerSlice";
+import { EffectData } from "../../../data/EffectData";
 import { CollapsibleList } from "../../components/ui";
 
 const EffectDisplay = (): ReactElement => {
@@ -9,39 +9,21 @@ const EffectDisplay = (): ReactElement => {
     const greatrune = useSelector(selectGreatrune);
     const greatruneactive = useSelector(selectGreatruneactive);
     const { talisman1, talisman2, talisman3, talisman4 } = useSelector(selectTalisman);
+    const { head, chest, hands, legs } = useSelector(selectArmor);
 
-    type greatRuneDataType = {
-        [key: string]: {effect: string}
-    }
-
-    const greatruneData: greatRuneDataType = {
-        "Godrick's Great Rune": {
-            effect: "Raises all attributes by 5"
-        },
-        "Malenia's Great Rune": {
-            effect: "Attacks recover HP after damage is taken"
-        },
-        "Radahn's Great Rune": {
-            effect: "Raises maximum HP, FP and stamina by 15%"
-        },
-        "Morgott's Great Rune": {
-            effect: "Greatly raises maximum HP by 25%"
-        },
-        "Rykard's Great Rune": {
-            effect: "Restores HP upon defeating enemies"
-        },
-        "Mohg's Great Rune": {
-            effect: "Grants a blessing of blood to phantoms"
-        }
-    };
+    const fallbackUnknownEffect = "unknown effect";
 
     const setEffectList = (): string[] => {
         let list = [];
-        greatruneactive && greatrune && list.push("Great Rune: " + greatruneData[greatrune]["effect"]);
-        talisman1 && list.push("Talisman 1: " + TalismansData[talisman1]["summary"]);
-        talisman2 && list.push("Talisman 2: " + TalismansData[talisman2]["summary"]);
-        talisman3 && list.push("Talisman 3: " + TalismansData[talisman3]["summary"]);
-        talisman4 && list.push("Talisman 4: " + TalismansData[talisman4]["summary"]);
+        greatruneactive && greatrune && list.push("Great Rune: " + (EffectData[greatrune]?.["Effects"] ?? fallbackUnknownEffect));
+        talisman1 && list.push("Talisman 1: " + (EffectData[talisman1]?.["Effects"] ?? fallbackUnknownEffect));
+        talisman2 && list.push("Talisman 2: " + (EffectData[talisman2]?.["Effects"] ?? fallbackUnknownEffect));
+        talisman3 && list.push("Talisman 3: " + (EffectData[talisman3]?.["Effects"] ?? fallbackUnknownEffect));
+        talisman4 && list.push("Talisman 4: " + (EffectData[talisman4]?.["Effects"] ?? fallbackUnknownEffect));
+        head && EffectData[head]?.["Effects"] && list.push("Head: " + EffectData[head]?.["Effects"]);
+        chest && EffectData[chest]?.["Effects"] && list.push("Chest: " + EffectData[chest]?.["Effects"]);
+        hands && EffectData[hands]?.["Effects"] && list.push("Hands: " + EffectData[hands]?.["Effects"]);
+        legs && EffectData[legs]?.["Effects"] && list.push("Legs: " + EffectData[legs]?.["Effects"]);
         return list
     };
 
