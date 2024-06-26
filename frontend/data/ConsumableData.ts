f8f87e203e90e31ad2046948e5fa67913855c2ae
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-06-26 16:03:46 +0200

    Adds comment to ConsumableData

--- frontend/data/ConsumableData.ts
+++ frontend/data/ConsumableData.ts
@@ -1,3 +1,11 @@
+/* 
+data source:
+1. ER - Build Planner spreadsheet
+2. table: ConsumableData
+3. get csv of table
+4. use https://csvjson.com/csv2json options: Parse numbers, Parse JSON, output: Hash
+*/
+
 type Data = {
     "AtkID": number,
     "attackBasePhysics": number,
