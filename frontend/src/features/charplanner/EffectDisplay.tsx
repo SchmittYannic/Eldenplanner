183074a3254abc88b6bba9e0a9574533a4cc3a19
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-06-26 16:38:32 +0200

    Updates EffectDisplay since prop name Effects changed to Ported Effects

--- frontend/src/features/charplanner/EffectDisplay.tsx
+++ frontend/src/features/charplanner/EffectDisplay.tsx
@@ -15,15 +15,15 @@ const EffectDisplay = (): ReactElement => {
 
     const setEffectList = (): string[] => {
         let list = [];
-        greatruneactive && greatrune && list.push("Great Rune: " + (EffectData[greatrune]?.["Effects"] ?? fallbackUnknownEffect));
-        talisman1 && list.push("Talisman 1: " + (EffectData[talisman1]?.["Effects"] ?? fallbackUnknownEffect));
-        talisman2 && list.push("Talisman 2: " + (EffectData[talisman2]?.["Effects"] ?? fallbackUnknownEffect));
-        talisman3 && list.push("Talisman 3: " + (EffectData[talisman3]?.["Effects"] ?? fallbackUnknownEffect));
-        talisman4 && list.push("Talisman 4: " + (EffectData[talisman4]?.["Effects"] ?? fallbackUnknownEffect));
-        head && EffectData[head]?.["Effects"] && list.push("Head: " + EffectData[head]?.["Effects"]);
-        chest && EffectData[chest]?.["Effects"] && list.push("Chest: " + EffectData[chest]?.["Effects"]);
-        hands && EffectData[hands]?.["Effects"] && list.push("Hands: " + EffectData[hands]?.["Effects"]);
-        legs && EffectData[legs]?.["Effects"] && list.push("Legs: " + EffectData[legs]?.["Effects"]);
+        greatruneactive && greatrune && list.push("Great Rune: " + (EffectData[greatrune]?.["Ported Effects"] ?? fallbackUnknownEffect));
+        talisman1 && list.push("Talisman 1: " + (EffectData[talisman1]?.["Ported Effects"] ?? fallbackUnknownEffect));
+        talisman2 && list.push("Talisman 2: " + (EffectData[talisman2]?.["Ported Effects"] ?? fallbackUnknownEffect));
+        talisman3 && list.push("Talisman 3: " + (EffectData[talisman3]?.["Ported Effects"] ?? fallbackUnknownEffect));
+        talisman4 && list.push("Talisman 4: " + (EffectData[talisman4]?.["Ported Effects"] ?? fallbackUnknownEffect));
+        head && EffectData[head]?.["Ported Effects"] && list.push("Head: " + EffectData[head]?.["Ported Effects"]);
+        chest && EffectData[chest]?.["Ported Effects"] && list.push("Chest: " + EffectData[chest]?.["Ported Effects"]);
+        hands && EffectData[hands]?.["Ported Effects"] && list.push("Hands: " + EffectData[hands]?.["Ported Effects"]);
+        legs && EffectData[legs]?.["Ported Effects"] && list.push("Legs: " + EffectData[legs]?.["Ported Effects"]);
         return list
     };
 
