9bddc7d7581c800b38d5b8c9db581b17942dc1ee
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-06-29 06:32:23 +0200

    Updates styles of spaces between headline, subtext and buttons on Frontpage

--- frontend/src/components/Herosection.tsx
+++ frontend/src/components/Herosection.tsx
@@ -19,13 +19,11 @@ const Herosection = (): ReactElement => {
                     <h1 className="gold-text-background">ELDENPLANNER</h1>
                     <div className="divider-4" />
                     <div className="divider-4" />
-                    <div className="divider-4" />
                     <p>
                         A Character Build Planner for the game <a href="https://de.bandainamcoent.eu/elden-ring/elden-ring" target="_blank">Elden Ring</a>. The perfect starting point for your next adventure through the Lands Between.
                     </p>
                     <div className="divider-4" />
-                    <div className="divider-4" />
-                    <div className="divider-4" />
+                    <div className="divider-2" />
                     <Link to={"/charplanner"} className="action-btn">
                         Get started
                     </Link>
