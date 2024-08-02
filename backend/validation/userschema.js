09a4a4345162731236931b31073cfc954d2a9d63
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-02 22:05:45 +0200

    Renames userschema into signupschema

--- backend/validation/userschema.js
+++ backend/validation/userschema.js
@@ -4,7 +4,6 @@ export const emailschema = Joi.string()
     .email()
     .pattern(new RegExp(/^\S.*\S$|^\S$/))
     .message("Email cannot contain leading or trailing spaces")
-    .required()
     .label("email")
     .messages({
         "string.email": "Invalid email format",
@@ -26,7 +25,6 @@ export const passwordschema = Joi.string()
     .message("Password must contain at least one digit")
     .pattern(new RegExp(/.*[!@#$%^&*_\-].*/))
     .message("Password must contain at least one special character")
-    .required()
     .messages({
         "string.empty": "Password is required",
         "any.required": "Password is required"
@@ -42,7 +40,6 @@ export const usernameschema = Joi.string()
     .message("Username must start with a letter")
     .pattern(new RegExp(/^[A-Za-z0-9_]+$/))
     .message("Usernames can only consist of letters, digits and underscores")
-    .required()
     .messages({
         "string.min": "Username must be atleast 4 characters long",
         "string.max": "Username cannot be more than 20 characters long",
@@ -51,8 +48,8 @@ export const usernameschema = Joi.string()
         "any.required": "Username is required",
     });
 
-export const userschema = Joi.object().keys({
-    username: usernameschema,
-    email: emailschema,
-    password: passwordschema,
+export const signupschema = Joi.object().keys({
+    username: usernameschema.required(),
+    email: emailschema.required(),
+    password: passwordschema.required(),
 });
