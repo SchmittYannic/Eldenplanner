a06a73bda9dd9c0c20c257103deb0642fbc80f4e
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-04 23:34:29 +0200

    Allows empty strings to pass through the passwordschema in case it isnt required

--- frontend/src/validation/userschema.ts
+++ frontend/src/validation/userschema.ts
@@ -10,13 +10,14 @@ const usernameschema = string()
 const emailschema = string().email("Invalid email format")
     .max(80, "Email cannot be more than 20 characters long")
 
+//regex in passwordschema allow empty strings, so it is usable for edituserschema aswell
 const passwordschema = string()
     .max(80, "Passwort darf nicht mehr als 80 Zeichen lang sein")
-    .matches(/^.{6,80}$/, "Password must be between 6 and 80 characters long")
-    .matches(/.*[A-Z].*/, "Password must contain at least one capital letter")
-    .matches(/.*[a-z].*/, "Password must contain at least one lowercase letter")
-    .matches(/.*\d.*/, "Password must contain at least one digit")
-    .matches(/.*[!@#$%^&*_\-].*/, "Password must contain at least one special character")
+    .matches(/^(.{6,80})?$/, "Password must be between 6 and 80 characters long")
+    .matches(/^(.*[A-Z].*|)$/, "Password must contain at least one capital letter")
+    .matches(/^(.*[a-z].*|)$/, "Password must contain at least one lowercase letter")
+    .matches(/^(.*\d.*|)$/, "Password must contain at least one digit")
+    .matches(/^(.*[!@#$%^&*_\-].*|)$/, "Password must contain at least one special character")
 
 const signupschema = object().shape({
     username: usernameschema.required("Username is required"),
@@ -24,6 +25,13 @@ const signupschema = object().shape({
     password: passwordschema.required("Password is required"),
 });
 
+const edituserschema = object().shape({
+    newUsername: usernameschema.required("Username is required"),
+    newEmail: emailschema.required("Email is required"),
+    newPassword: passwordschema,
+});
+
 export {
     signupschema,
+    edituserschema,
 }
