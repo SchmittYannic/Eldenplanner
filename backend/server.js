fb1567d94212cbf8c9c320b55fff7805dd3a0384
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-05 04:12:01 +0200

    Changes quotation marks

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import 'express-async-errors';
+import "express-async-errors";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
