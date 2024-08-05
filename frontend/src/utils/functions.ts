ebcadc61de01b9ed4cdb9f2d9780e5aa720844b7
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-05 23:44:57 +0200

    Adds helper function sinceDateInString

--- frontend/src/utils/functions.ts
+++ frontend/src/utils/functions.ts
@@ -89,6 +89,35 @@ const calcStatChange = (
     return headEffect + chestEffect + handsEffect + legsEffect + talisman1Effect + talisman2Effect + talisman3Effect + talisman4Effect + greatRuneEffect
 };
 
+const sinceDateInString = (eventTime: Date): string => {
+    const now = new Date();
+    const diff = now.getTime() - eventTime.getTime();
+
+    const seconds = Math.floor(diff / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const weeks = Math.floor(days / 7);
+    const months = Math.floor(days / 30);
+    const years = Math.floor(days / 365);
+
+    if (seconds < 60) {
+        return `${seconds} seconds ago`;
+    } else if (minutes < 60) {
+        return `${minutes} minutes ago`;
+    } else if (hours < 24) {
+        return `${hours} hours ago`;
+    } else if (days < 7) {
+        return `${days} days ago`;
+    } else if (weeks < 4) {
+        return `${weeks} weeks ago`;
+    } else if (months < 12) {
+        return `${months} months ago`;
+    } else {
+        return `${years} years ago`;
+    }
+}
+
 export {
     calcRuneLevel,
     calcNextLevelRunes,
@@ -97,4 +126,5 @@ export {
     capitalizeFirstLetter,
     calcSumObjectValues,
     calcStatChange,
+    sinceDateInString,
 }
