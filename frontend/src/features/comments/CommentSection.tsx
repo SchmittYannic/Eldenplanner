2f9d96ebc86bd0c43af781ac63b28ffab1b7ccee
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-07 04:40:31 +0200

    Updates CommentSection to use commentsSlice instead of local state

--- frontend/src/features/comments/CommentSection.tsx
+++ frontend/src/features/comments/CommentSection.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
-import { useGetCommentsQuery } from "src/features/comments/commentApiSlice";
+import { useGetCommentsQuery } from "src/features/comments/commentsApiSlice";
+import { selectAllComments, selectLastFetchedTimestamp, selectLimit, selectSort, selectTotalComments } from "./commentsSlice";
 import CommentBox from "./CommentBox";
 import Comment from "src/features/comments/Comment";
-import { CommentType, SortCommentsType, TargetTypeType } from "src/types";
+import { TargetTypeType } from "src/types";
 import "src/features/comments/CommentSection.scss";
 
 type CommentSectionPropsType = {
@@ -16,22 +18,12 @@ const CommentSection = ({
     targetType,
 }: CommentSectionPropsType) => {
 
-    const [totalComments, setTotalComments] = useState(0);
-    const [comments, setComments] = useState<CommentType[]>([]);
-    const [hasMore, setHasMore] = useState<boolean>(true);
-    const [lastFetchedTimestamp, setLastFetchedTimestamp] = useState<string>("");
-    const [sort, setSort] = useState<SortCommentsType>("new");
-    const [limit, setLimit] = useState<number>(25);
+    const lastFetchedTimestamp = useSelector(selectLastFetchedTimestamp);
+    const sort = useSelector(selectSort);
+    const limit = useSelector(selectLimit);
+    const totalComments = useSelector(selectTotalComments);
 
-    const [showCommentBoxFooter, setShowCommentBoxFooter] = useState(false);
-
-    const {
-        data,
-        isFetching,
-        isError,
-        isSuccess,
-        refetch,
-    } = useGetCommentsQuery({
+    useGetCommentsQuery({
         targetId,
         targetType,
         lastFetchedTimestamp,
@@ -39,28 +31,9 @@ const CommentSection = ({
         limit,
     });
 
-    useEffect(() => {
-        if (data) {
-            const { comments: newComments, totalComments } = data;
-            setTotalComments(totalComments);
+    const comments = useSelector(selectAllComments);
 
-            if (newComments.length > 0) {
-                setComments((prev) => {
-                    const existingIds = new Set(prev.map(comment => comment.id));
-                    const uniqueNewComments = newComments.filter(comment => !existingIds.has(comment.id));
-                    return [...prev, ...uniqueNewComments];
-                });
-
-                // Update lastFetchedTimestamp based on the last comment's timestamp in the new data
-                setLastFetchedTimestamp(newComments[newComments.length - 1]?.createdAt || "");
-
-                // Check if there are more comments to fetch
-                if (comments.length >= totalComments) {
-                    setHasMore(false);
-                }
-            }
-        }
-    }, [data]);
+    const [showCommentBoxFooter, setShowCommentBoxFooter] = useState(false);
 
     const onCommentBoxCancelClicked = () => {
         setShowCommentBoxFooter(false);
