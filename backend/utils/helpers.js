f0ab006300d18bdb6245cc1823126d840428cb55
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-07 18:59:24 +0200

    Adds helper function to transform comments in backend before sending to frontend

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -2,3 +2,22 @@ export const parseError = err => {
     if (err.isJoi) return err.details[0];
     return JSON.stringify(err, Object.getOwnPropertyNames(err));
 };
+
+export const getIdsAndEntitiesOfComments = (comments) => {
+    // Check if input is an array
+    if (!Array.isArray(comments)) return { ids: [], entities: {} }
+
+    // Check if the array is not empty
+    if (comments.length === 0) return { ids: [], entities: {} }
+    const ids = [];
+    const entities = {};
+
+    comments.forEach(comment => {
+        if (typeof comment !== "object" || comment === null) return { ids: [], entities: {} }
+        if (!comment.hasOwnProperty("id") || typeof comment.id !== "string") return { ids: [], entities: {} }
+        ids.push(comment.id);
+        entities[comment.id] = comment;
+    });
+
+    return { ids, entities };
+}
