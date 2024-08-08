704215440b042f5ab59fef5e0267546d3584e539
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-08 21:08:34 +0200

    Adds intersection observer to load more comments

--- frontend/src/features/comments/CommentSection.tsx
+++ frontend/src/features/comments/CommentSection.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 import { useLazyGetCommentsQuery } from "src/features/comments/commentsApiSlice";
-import { selectAllCommentIds, selectLastFetchedTimestamp, selectLimit, selectSort, selectTotalComments } from "./commentsSlice";
+import { selectAllCommentIds, selectHasMoreComments, selectLastFetchedTimestamp, selectLimit, selectSort, selectTotalComments } from "./commentsSlice";
 import CommentBox from "./CommentBox";
 import Comment from "src/features/comments/Comment";
 import { TargetTypeType } from "src/types";
@@ -21,10 +21,11 @@ const CommentSection = ({
     const lastFetchedTimestamp = useSelector(selectLastFetchedTimestamp);
     const sort = useSelector(selectSort);
     const limit = useSelector(selectLimit);
+    const hasMoreComments = useSelector(selectHasMoreComments);
     const totalComments = useSelector(selectTotalComments);
 
-    const [fetchComment, {
-        isLoading,
+    const [fetchComments, {
+        isFetching,
     }] = useLazyGetCommentsQuery();
 
     // useGetCommentsQuery({
@@ -38,7 +39,10 @@ const CommentSection = ({
 
     const commentIds = useSelector(selectAllCommentIds);
 
+    const observer = useRef<IntersectionObserver | null>(null);
+    const observerRef = useRef<HTMLDivElement>(null);
     const [showCommentBoxFooter, setShowCommentBoxFooter] = useState(false);
+    const [debounceTimeout, setDebounceTimeout] = useState<number | null>(null);
 
     const onCommentBoxCancelClicked = () => {
         setShowCommentBoxFooter(false);
@@ -49,7 +53,7 @@ const CommentSection = ({
     };
 
     useEffect(() => {
-        fetchComment({
+        fetchComments({
             targetId,
             targetType,
             parentId: "",
@@ -57,7 +61,56 @@ const CommentSection = ({
             sort,
             limit,
         })
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        if (observer.current) observer.current.disconnect();
+
+        const handleIntersection = () => {
+            if (hasMoreComments && !isFetching) {
+                fetchComments({
+                    targetId,
+                    targetType,
+                    parentId: "",
+                    lastFetchedTimestamp,
+                    sort,
+                    limit,
+                });
+            }
+        };
+
+        const debouncedFetch = () => {
+            if (debounceTimeout) clearTimeout(debounceTimeout);
+
+            const timeout = setTimeout(() => {
+                handleIntersection();
+            }, 300);
+
+            setDebounceTimeout(Number(timeout));
+        };
+
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting) {
+                debouncedFetch();
+            }
+        });
+
+        if (observerRef.current) observer.current.observe(observerRef.current);
+
+        return () => {
+            if (observer.current) observer.current.disconnect();
+            if (debounceTimeout) clearTimeout(debounceTimeout);
+        };
+    }, [
+        hasMoreComments,
+        isFetching,
+        fetchComments,
+        targetId,
+        targetType,
+        lastFetchedTimestamp,
+        sort,
+        limit
+    ]);
 
     return (
         <section className="CommentSection">
@@ -84,6 +137,9 @@ const CommentSection = ({
                     />
                 ))}
             </div>
+
+            <div id="load-more" ref={observerRef} />
+            {isFetching && <p>Loading more comments...</p>}
         </section>
     )
 }
