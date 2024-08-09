8f817a7738aa91a2a997c917ee02a5acb0d737b1
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-09 23:35:14 +0200

    Adds new types

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -32,30 +32,47 @@ export type TargetTypeType = "Build" | "User";
 export const sortOptions = ["new", "old", "popular"] as const;
 export type SortCommentsType = typeof sortOptions[number];
 
+const likeTypeOptions = ["like", "dislike"] as const;
+export type LikeTypeType = typeof likeTypeOptions[number];
 
 export type CommentType<CommentId extends string = string, ReplyId extends string = string> = {
-    id: CommentId;
-    authorId: string;
-    username: string;
+    id: CommentId,
+    authorId: string,
+    username: string,
     avatarUrl: string,
-    parentId: string | null;
-    targetId: string;
-    targetType: TargetTypeType;
-    content: string;
-    totalReplies: number;
-    likes: number;
-    dislikes: number;
-    createdAt: string;
-    updatedAt: string;
-    repliesIds?: ReplyId[];
-    repliesEntities?: Record<ReplyId, CommentType<ReplyId>>;
-    lastReplyFetchedTimestamp?: string;
-    hasMoreReplies?: boolean;
-    hasLiked?: boolean;
+    parentId: string | null,
+    targetId: string,
+    targetType: TargetTypeType,
+    content: string,
+    totalReplies: number,
+    likes: number,
+    dislikes: number,
+    createdAt: string,
+    updatedAt: string,
+    repliesIds?: ReplyId[],
+    repliesEntities?: Record<ReplyId, CommentType<ReplyId>>,
+    lastReplyFetchedTimestamp?: string,
+    hasMoreReplies?: boolean,
+    hasLiked?: boolean,
+    hasDisliked?: boolean,
 }
 
 export type GetCommentsResponseType<CommentId extends string> = {
-    ids: CommentId[];
-    entities: Record<CommentId, CommentType<CommentId>>;
-    totalComments: number;
+    ids: CommentId[],
+    entities: Record<CommentId, CommentType<CommentId>>,
+    totalComments: number,
 };
+
+export type GetCommentsQueryParamsType = {
+    targetId: string,
+    targetType: string,
+    parentId: string,
+    lastFetchedTimestamp: string,
+    sort?: SortCommentsType,
+    limit?: number,
+}
+
+export type AddLikeDislikeMutationParamsType = {
+    commentId: string,
+    type: LikeTypeType,
+} & GetCommentsQueryParamsType
