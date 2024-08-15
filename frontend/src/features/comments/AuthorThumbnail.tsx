a5816359b4f37965d3350832a5d959661007a269
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-15 16:36:50 +0200

    Updates AuthorThumbnail making href prop optional

--- frontend/src/features/comments/AuthorThumbnail.tsx
+++ frontend/src/features/comments/AuthorThumbnail.tsx
@@ -2,25 +2,35 @@ import { ImgHTMLAttributes } from "react";
 import { Link } from "react-router-dom";
 
 type AuthorThumbnailPropsType = {
-    href: string,
+    href?: string,
 } & ImgHTMLAttributes<HTMLImageElement>
 
 const AuthorThumbnail = ({
     href,
     ...rest
 }: AuthorThumbnailPropsType) => {
-    return (
-        <div className="author-thumbnail">
-            <Link
-                className="img-link"
-                to={href}
-            >
+    if (href) {
+        return (
+            <div className="author-thumbnail">
+                <Link
+                    className="img-link"
+                    to={href}
+                >
+                    <div>
+                        <img {...rest} />
+                    </div>
+                </Link>
+            </div>
+        )
+    } else {
+        return (
+            <div className="author-thumbnail">
                 <div>
                     <img {...rest} />
                 </div>
-            </Link>
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 export default AuthorThumbnail
