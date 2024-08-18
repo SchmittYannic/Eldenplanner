cd39bce37685b34efda80e07d9a1399ffb26b2ad
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-18 19:03:11 +0200

    Changes to buildRoutes using new getBuilds for route /builds GET

--- backend/routes/buildRoutes.js
+++ backend/routes/buildRoutes.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { getAllBuilds, createNewBuild, updateBuild, deleteBuild } from "../controllers/buildsController.js";
+import { getAllBuilds, createNewBuild, updateBuild, deleteBuild, getBuilds } from "../controllers/buildsController.js";
 import checkBuildData from "../middleware/checkBuildData.js";
 import verifyJWT from "../middleware/verifyJWT.js";
 const router = express.Router();
 
 router.route("/")
-    .get(getAllBuilds)
+    .get(getBuilds)
     .post(verifyJWT, checkBuildData, createNewBuild)
     .patch(verifyJWT, checkBuildData, updateBuild)
     .delete(verifyJWT, deleteBuild);
+
+router.route("/old")
+    .get(getAllBuilds)
 
 export default router;
