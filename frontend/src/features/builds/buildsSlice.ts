d449c46ffea3c06a28d2885ea1994de77173372a
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-18 20:51:44 +0200

    Updates buildsSlice with ColumnFilterState

--- frontend/src/features/builds/buildsSlice.ts
+++ frontend/src/features/builds/buildsSlice.ts
@@ -1,12 +1,15 @@
 import { createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { PaginationState, SortingState } from "@tanstack/react-table";
+import { ColumnFiltersState, PaginationState, SortingState } from "@tanstack/react-table";
+
 import { RootState } from "src/app/store";
 import { OrderType } from "src/types";
+import { findObjectById, isFilterColumnValueArray } from "src/utils/functions";
 
 interface BuildsStateType {
     pageSize: number,
     pageIndex: number,
     sorting: SortingState,
+    columnFilter: ColumnFiltersState,
 }
 
 const initialState: BuildsStateType = {
@@ -15,6 +18,7 @@ const initialState: BuildsStateType = {
     sorting: [
         { id: "stars", desc: false }
     ],
+    columnFilter: [],
 };
 
 export const buildsSlice = createSlice({
@@ -29,12 +33,16 @@ export const buildsSlice = createSlice({
         setBuildsSorting: (state, { payload }: PayloadAction<SortingState>) => {
             state.sorting = payload;
         },
+        setBuildsColumnFilter: (state, { payload }: PayloadAction<ColumnFiltersState>) => {
+            state.columnFilter = payload;
+        },
     },
 });
 
 export const {
     setBuildsPagination,
     setBuildsSorting,
+    setBuildsColumnFilter,
 } = buildsSlice.actions;
 
 export const selectBuildsPagination = createSelector(
@@ -48,6 +56,7 @@ export const selectBuildsPagination = createSelector(
 export const selectBuildsLimit = (state: RootState) => state.builds.pageSize;
 export const selectBuildsSkip = (state: RootState) => state.builds.pageSize * state.builds.pageIndex;
 export const selectBuildsSorting = (state: RootState) => state.builds.sorting;
+export const selectBuildsColumnFilter = (state: RootState) => state.builds.columnFilter;
 
 export const selectBuildsOrder = (state: RootState) => {
     const order: OrderType = !state.builds.sorting.length ? "asc" : state.builds.sorting[0].desc ? "desc" : "asc";
@@ -57,5 +66,41 @@ export const selectBuildsField = (state: RootState) => {
     const field = state.builds.sorting.length ? state.builds.sorting[0].id : "stars";
     return field
 };
+
+export const selectBuildsTitleFilter = (state: RootState) => {
+    const titleFilter = findObjectById(state.builds.columnFilter, "title");
+    const title = titleFilter && typeof titleFilter.value === "string" ? titleFilter.value : undefined;
+    return title
+}
+
+export const selectBuildsUsernameFilter = (state: RootState) => {
+    const usernameFilter = findObjectById(state.builds.columnFilter, "username");
+    const username = usernameFilter && typeof usernameFilter.value === "string" ? usernameFilter.value : undefined;
+    return username
+}
+
+export const selectBuildsLevelFilter = createSelector(
+    [selectBuildsColumnFilter],
+    (columnFilter) => {
+        const levelFilter = findObjectById(columnFilter, "level");
+        const level = levelFilter && isFilterColumnValueArray(levelFilter.value) ? levelFilter.value : undefined;
+        return {
+            minLevel: level === undefined ? "0" : level[0] === null ? "0" : level[0],
+            maxLevel: level === undefined ? undefined : level[1],
+        };
+    }
+);
+
+export const selectBuildsStarsFilter = createSelector(
+    [selectBuildsColumnFilter],
+    (columnFilter) => {
+        const starsFilter = findObjectById(columnFilter, "stars");
+        const stars = starsFilter && isFilterColumnValueArray(starsFilter.value) ? starsFilter.value : undefined;
+        return {
+            minStars: stars === undefined ? "0" : stars[0] === null ? "0" : stars[0],
+            maxStars: stars === undefined ? undefined : stars[1],
+        };
+    }
+);
 
 export default buildsSlice.reducer;
