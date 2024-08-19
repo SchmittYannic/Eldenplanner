0a7445f7302d786ee8e7ef9acfe7db2ba804258a
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-19 02:21:13 +0200

    Reset buildsSlice on dismount of Build component

--- frontend/src/features/builds/Builds.tsx
+++ frontend/src/features/builds/Builds.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -12,6 +12,7 @@ import {
 
 import { useGetBuildsQuery } from "./buildsApiSlice";
 import {
+    resetBuildsSliceState,
     selectBuildsColumnFilter,
     selectBuildsField,
     selectBuildsLevelFilter,
@@ -148,6 +149,12 @@ const BuildsNew = () => {
             },
         ], []
     );
+
+    useEffect(() => {
+        return () => {
+            dispatch(resetBuildsSliceState());
+        }
+    }, []);
 
     return (
         <BuildsTable
