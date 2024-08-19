1d47c60503c5717e5144c999fa558c8c5949323f
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-19 20:01:07 +0200

    Removes error text on initial render in UserPage

--- frontend/src/features/users/UserPage.tsx
+++ frontend/src/features/users/UserPage.tsx
@@ -159,13 +159,9 @@ const UserPage = (): ReactElement => {
         )
     }
 
-    // should never show this
+    // should only show this briefly on mount when fetch is uninitialized, no cache data exists and user is undefined
     return (
         <main className="main--userpage">
-            <div className="sm-alert errmsg">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                <span>Something went wrong.</span>
-            </div>
         </main>
     )
 }
