aa6582aba0aaf4cd5e2737a4cd493b6fe8c87622
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-19 23:01:53 +0200

    Updates Users component

--- frontend/src/features/users/Users.tsx
+++ frontend/src/features/users/Users.tsx
@@ -6,8 +6,8 @@ import UsersList from "./UsersList";
 import EditUserAsAdmin from "./EditUserAsAdmin";
 import DeleteUserAsAdmin from "./DeleteUserAsAdmin";
 import { ClipLoader } from "src/components/ui";
+import { isCustomError } from "src/utils/typeguards";
 import { UserAsAdminType } from "src/types";
-import { isCustomError, isUserAsAdminType } from "src/utils/typeguards";
 
 const Users = (): ReactElement => {
 
@@ -22,21 +22,9 @@ const Users = (): ReactElement => {
     });
 
     const params = useParams();
-    const user = params.userId ? users?.entities[params.userId] : null;
+    const user = params.userId ? users?.entities[params.userId] : undefined;
 
-    const tableData = isSuccess && users.ids.map(id => {
-        return users.entities[id] as UserAsAdminType
-    })
-
-    if (tableData) {
-        return (
-            <>
-                <UsersList data={tableData} />
-                {isUserAsAdminType(user) && params.action === "edit" && <EditUserAsAdmin user={user} />}
-                {isUserAsAdminType(user) && params.action === "delete" && <DeleteUserAsAdmin user={user} />}
-            </>
-        )
-    } else if (isLoading) {
+    if (isLoading) {
         return (
             <main>
                 <ClipLoader
@@ -46,7 +34,9 @@ const Users = (): ReactElement => {
                 />
             </main>
         )
-    } else if (isError) {
+    }
+
+    if (isError) {
         const errormsg = isCustomError(error) && (error.status === 400 || error.status === 401) && error.data.message;
         return (
             <main>
@@ -56,16 +46,29 @@ const Users = (): ReactElement => {
                 </div>
             </main>
         )
-    } else {
+    }
+
+    if (isSuccess) {
+        const tableData = users.ids.reduce<UserAsAdminType[]>((acc, id) => {
+            const user = users.entities[id];
+            if (user) {
+                acc.push(user);
+            }
+            return acc;
+        }, []);
         return (
-            <main>
-                <div className="sm-alert errmsg">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                    <span>Something went wrong.</span>
-                </div>
-            </main>
+            <>
+                <UsersList data={tableData} />
+                {user && params.action === "edit" && <EditUserAsAdmin user={user} />}
+                {user && params.action === "delete" && <DeleteUserAsAdmin user={user} />}
+            </>
         )
     }
+
+    return (
+        <main>
+        </main>
+    )
 }
 
 export default Users
