cf1b83470218862eaa10d0e447a15b014322a9f0
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-21 16:10:11 +0200

    Adds allowedOrigin for cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://eldenplanner.onrender.com"
+    "https://eldenplanner.onrender.com",
+    "https://eldenplanner.de",
 ];
 
 export default allowedOrigins;
