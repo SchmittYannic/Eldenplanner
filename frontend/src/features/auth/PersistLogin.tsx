e7cf1dbe6c7874540b12db5637eb756ed9356fa6
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-08-21 15:41:20 +0200

    Updates PersistLogin

--- frontend/src/features/auth/PersistLogin.tsx
+++ frontend/src/features/auth/PersistLogin.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import { useRefreshMutation } from "./authApiSlice";
 import { selectCurrentToken } from "./authSlice";
 import usePersist from "src/hooks/usePersist";
-import { ClipLoader } from "src/components/ui";
 
 const PersistLogin = () => {
 
@@ -13,33 +12,26 @@ const PersistLogin = () => {
     const token = useSelector(selectCurrentToken);
     const effectRan = useRef(false);
 
-    const [trueSuccess, setTrueSuccess] = useState(false);
+    //const [trueSuccess, setTrueSuccess] = useState(false);
 
-    const [refresh, {
-        isUninitialized,
-        isLoading,
-        isSuccess,
-        isError,
-    }] = useRefreshMutation();
+    const [refresh] = useRefreshMutation();
 
     useEffect(() => {
 
         if (effectRan.current === true || process.env.NODE_ENV !== "development") { // React 18 Strict Mode
 
-            const verifyRefreshToken = async () => {
-                //console.log("verifying refresh token")
-                try {
-                    //const response = 
-                    await refresh("").unwrap();
-                    //const { accessToken } = response.data
-                    setTrueSuccess(true);
-                }
-                catch (err) {
+            // const verifyRefreshToken = async () => {
+            //     try {
+            //         await refresh("").unwrap();
 
-                }
-            }
+            //         //setTrueSuccess(true);
+            //     }
+            //     catch (err) {
 
-            if (!token && persist) verifyRefreshToken()
+            //     }
+            // }
+
+            if (!token && persist) refresh("");
         }
 
         return () => { effectRan.current = true }
@@ -47,42 +39,42 @@ const PersistLogin = () => {
         // eslint-disable-next-line
     }, []);
 
+    return <Outlet />
+    // let content;
+    // if (!persist) { // persist: no
+    //     //console.log("no persist")
+    //     content = <Outlet />
+    // } else if (isLoading) { //persist: yes, token: no
+    //     //console.log("loading")
+    //     content = (
+    //         <main className="page-spinner">
+    //             <ClipLoader
+    //                 color={"rgb(231, 214, 182)"}
+    //                 loading={isLoading}
+    //                 size={30}
+    //             />
+    //         </main>
+    //     )
+    // } else if (isError) { //persist: yes, token: no
+    //     // console.log("error")
+    //     // content = (
+    //     //     <main>
+    //     //         <div className="sm-alert errmsg">
+    //     //             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+    //     //             <span>Something went wrong. <Link to="/login">Please login again</Link></span>
+    //     //         </div>
+    //     //     </main>
+    //     // )
+    //     //console.log("not logged in")
+    //     content = <Outlet />
+    // } else if (isSuccess && trueSuccess) { //persist: yes, token: yes
+    //     //console.log("success")
+    //     content = <Outlet />
+    // } else if (token && isUninitialized) { //persist: yes, token: yes
+    //     //console.log("token and uninit")
+    //     content = <Outlet />
+    // }
 
-    let content;
-    if (!persist) { // persist: no
-        //console.log("no persist")
-        content = <Outlet />
-    } else if (isLoading) { //persist: yes, token: no
-        //console.log("loading")
-        content = (
-            <main className="page-spinner">
-                <ClipLoader
-                    color={"rgb(231, 214, 182)"}
-                    loading={isLoading}
-                    size={30}
-                />
-            </main>
-        )
-    } else if (isError) { //persist: yes, token: no
-        // console.log("error")
-        // content = (
-        //     <main>
-        //         <div className="sm-alert errmsg">
-        //             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-        //             <span>Something went wrong. <Link to="/login">Please login again</Link></span>
-        //         </div>
-        //     </main>
-        // )
-        //console.log("not logged in")
-        content = <Outlet />
-    } else if (isSuccess && trueSuccess) { //persist: yes, token: yes
-        //console.log("success")
-        content = <Outlet />
-    } else if (token && isUninitialized) { //persist: yes, token: yes
-        //console.log("token and uninit")
-        content = <Outlet />
-    }
-
-    return content
+    // return content
 }
 export default PersistLogin
