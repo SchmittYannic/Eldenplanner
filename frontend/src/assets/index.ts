4fb19576c2c9312714bde3b6051e2952d3d2bfb5
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-09-18 22:18:08 +0200

    Moves charplannerpng and buildspng to public folder

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,11 +1,7 @@
 import navbarlogo from "./navbarlogo.svg";
 import herobg from "./herobg.svg";
-import charplannerpng from "./charplannerpng.png";
-import buildspng from "./buildspng.png";
 
 export {
     navbarlogo,
     herobg,
-    charplannerpng,
-    buildspng,
 }
--- frontend/src/components/Cardscrollreveal.tsx
+++ frontend/src/components/Cardscrollreveal.tsx
@@ -1,11 +1,6 @@
 import { useEffect, useRef } from "react";
-import { buildspng, charplannerpng } from "../assets";
-import useWindowSize from "../hooks/useWindowSize";
 
 const Cardscrollreveal = () => {
-
-    const windowSize = useWindowSize();
-    const isMobile = windowSize.width && windowSize.width < 850;
 
     const containerRef = useRef<HTMLDivElement>(null);
     const imgBelowWrapperRef = useRef<HTMLDivElement>(null);
@@ -22,12 +17,12 @@ const Cardscrollreveal = () => {
                 const img2MarginTop = 50; // img margin top in px
                 const img2DifToTop = img2MarginTop + img2HeightNoScale * (1 - img2Scale); // img distance to top border of container
                 const bottomPosition = (windowHeight - 700) / 2; // img bottom position in css
-                
+
                 const img2scrolledinpx = ((windowHeight - bottomPosition) - (heightOfContentBefore + img2DifToTop + img2Height - scrollY));
-                const img2scrolledinpercent = (img2scrolledinpx / img2HeightNoScale) < 0 ? 0 
-                : (img2scrolledinpx / img2HeightNoScale) > 1 ? 1 
-                : (img2scrolledinpx / img2HeightNoScale);
-                
+                const img2scrolledinpercent = (img2scrolledinpx / img2HeightNoScale) < 0 ? 0
+                    : (img2scrolledinpx / img2HeightNoScale) > 1 ? 1
+                        : (img2scrolledinpx / img2HeightNoScale);
+
                 containerRef.current.style.setProperty("--scroll", (img2scrolledinpercent).toString());
             }
         }
@@ -42,7 +37,6 @@ const Cardscrollreveal = () => {
             <div className="cardscrollreveal-container" ref={containerRef}>
                 <div className="card-wrapper card-above">
                     <div className="card-border-top border-bg-img-1" />
-                    {!isMobile && <img src={charplannerpng} alt="image of charplanner" loading="lazy" />}
                     <div className="card-text-wrapper">
                         <h2>All in one</h2>
                         <p>
@@ -53,7 +47,6 @@ const Cardscrollreveal = () => {
                 </div>
                 <div className="card-wrapper card-below" ref={imgBelowWrapperRef}>
                     <div className="card-border-top border-bg-img-2" />
-                    {!isMobile && <img src={buildspng} alt="image of community builds" loading="lazy" />}
                     <div className="card-text-wrapper">
                         <h2>Community</h2>
                         <p>
