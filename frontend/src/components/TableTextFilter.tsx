6e78c9473798a2e3c53aa05f6699a8d365ec1692
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-09-18 03:49:33 +0200

    Adds TableTextFilter

---
+++ frontend/src/components/TableTextFilter.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Column } from "@tanstack/react-table";
+import { DebouncedInput } from "./ui";
+
+type TableTextFilterPropsType = {
+    column: Column<any, unknown>,
+}
+
+const TableTextFilter = ({ column }: TableTextFilterPropsType) => {
+
+    const columnFilterValue = column.getFilterValue();
+    const initialState = typeof columnFilterValue === "string" ? columnFilterValue : "";
+    const [inputValue, setInputValue] = useState(initialState);
+
+    return (
+        <div className="table--filter--slot">
+            <label htmlFor={column.id + "filter"} className="sr-only">
+                {`filter Column ${column.id}`}
+            </label>
+            <DebouncedInput
+                id={column.id + "filter"}
+                type="text"
+                value={inputValue}
+                onChange={(value) => {
+                    column.setFilterValue(value);
+                    setInputValue(String(value));
+                }}
+                placeholder={`Filter ${column.id}`}
+                className={`table--filter-text ${column.id}`}
+                title={`Filter column: ${column.id}`}
+            />
+        </div>
+    )
+};
+
+export default TableTextFilter
