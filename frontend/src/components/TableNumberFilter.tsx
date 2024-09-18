121068ff868bc2c4028d8e613dae4d0961b4aa75
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-09-18 03:49:23 +0200

    Adds TableNumberFilter

---
+++ frontend/src/components/TableNumberFilter.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { Column } from "@tanstack/react-table";
+import { DebouncedInput } from "./ui";
+
+type TableNumberFilterPropsType = {
+    column: Column<any, unknown>,
+}
+
+const TableNumberFilter = ({ column }: TableNumberFilterPropsType) => {
+
+    const columnFilterValue = column.getFilterValue();
+    const initialMinState = Array.isArray(columnFilterValue) ? columnFilterValue[0] : "";
+    const initialMaxState = Array.isArray(columnFilterValue) ? columnFilterValue[1] : "";
+    const [minValue, setMinValue] = useState(String(initialMinState));
+    const [maxValue, setMaxValue] = useState(String(initialMaxState));
+
+    const handleMinValueChange = (value: string | number) => {
+        column.setFilterValue([value, maxValue]);
+        setMinValue(String(value))
+    };
+
+    const handleMaxValueChange = (value: string | number) => {
+        column.setFilterValue([minValue, value]);
+        setMaxValue(String(value))
+    };
+
+    return (
+        <div className="table--filter--slot">
+            <div className="table--filter-number-wrapper">
+                <label htmlFor={column.id + "filterMin"} className="sr-only">
+                    {`filter column ${column.id} minimum`}
+                </label>
+                <DebouncedInput
+                    id={column.id + "filterMin"}
+                    type="number"
+                    min={0}
+                    value={minValue}
+                    onChange={handleMinValueChange}
+                    placeholder={`Min ${column.id}`}
+                    className="table--filter-number"
+                    title={`Filter by minimum ${column.id}`}
+                />
+                <label htmlFor={column.id + "filterMax"} className="sr-only">
+                    {`filter column ${column.id} maximum`}
+                </label>
+                <DebouncedInput
+                    id={column.id + "filterMax"}
+                    type="number"
+                    min={0}
+                    value={maxValue}
+                    onChange={handleMaxValueChange}
+                    placeholder={`Max ${column.id}`}
+                    className="table--filter-number"
+                    title={`Filter by maximum ${column.id}`}
+                />
+            </div>
+        </div>
+    )
+};
+
+export default TableNumberFilter
