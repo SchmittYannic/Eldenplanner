1e2350aa5dfd6d12dc55e4050f755f7e6fc00f0b
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-09-20 23:43:14 +0200

    Adds Star button to ActionsSection

--- frontend/src/features/charplanner/ActionsSection.tsx
+++ frontend/src/features/charplanner/ActionsSection.tsx
@@ -1,21 +1,39 @@
 import { ReactElement, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import { selectAuthorId } from "./charplannerSlice";
+import { useAddStarMutation, useDeleteStarMutation } from "./charplannerApiSlice";
+import { addToast } from "src/features/toasts/toastSlice";
+import { selectAuthorId, selectBuildId, selectHasGivenStar, selectStars } from "./charplannerSlice";
 import useAuth from "src/hooks/useAuth";
 import SaveBuild from "./SaveBuild";
 import DeleteBuild from "./DeleteBuild";
+import { MdOutlineStar, MdOutlineStarBorder } from "src/components/icons";
 
 
 const ActionsSection = (): ReactElement => {
 
+    const dispatch = useDispatch()
+    const buildId = useSelector(selectBuildId);
     const authorId = useSelector(selectAuthorId);
+    const stars = useSelector(selectStars);
+    const hasGivenStar = useSelector(selectHasGivenStar);
     const { userId } = useAuth();
+
+    const [addStar] = useAddStarMutation();
+    const [deleteStar] = useDeleteStarMutation();
 
     const [isDeleteTriggered, setIsDeleteTriggered] = useState(false);
     const [isSaveTriggered, setIsSaveTriggered] = useState(false);
 
     const isBuildAuthor = authorId === userId;
+
+    const starButtonTitle = isBuildAuthor
+        ? `Author cannot star own Build (${stars})`
+        : !userId
+            ? `Starring Builds requires login (${stars})`
+            : hasGivenStar
+                ? `Unstar this Build (${stars})`
+                : `Star this Build (${stars})`
 
     const onSaveClicked = () => {
         if (!userId) return
@@ -24,8 +42,64 @@ const ActionsSection = (): ReactElement => {
 
     const onDeleteClick = () => setIsDeleteTriggered(true);
 
+    const onStarClicked = async () => {
+        if (!buildId) return
+        if (!userId) return
+        if (isBuildAuthor) return
+        try {
+            if (hasGivenStar) {
+                await deleteStar({
+                    buildId,
+                    userId,
+                }).unwrap()
+            } else {
+                await addStar({
+                    buildId,
+                    userId,
+                }).unwrap();
+            }
+        } catch (err) {
+            dispatch(addToast({
+                type: "error",
+                text: `${hasGivenStar ? "Removing" : "Adding"} star failed`,
+            }));
+        }
+    }
+
     return (
         <section className="charplanner__actionscontainer">
+            {stars !== null &&
+                <button
+                    className="button"
+                    type="button"
+                    onClick={onStarClicked}
+                    title={starButtonTitle}
+                    // disabled for the author and non authenticated users
+                    disabled={isBuildAuthor || userId === ""}
+                >
+                    <span className="icon-container">
+                        {hasGivenStar ?
+                            <MdOutlineStar
+                                height={20}
+                                color="hsl(46.6 39.13% 68.43% / 1)"
+                                aria-hidden
+                            />
+                            :
+                            <MdOutlineStarBorder
+                                height={20}
+                                aria-hidden
+                            />
+                        }
+                    </span>
+                    <span>
+                        {hasGivenStar ? "Starred" : "Star"}&nbsp;
+                    </span>
+                    <span>
+                        {stars}
+                    </span>
+                </button>
+            }
+
             {isBuildAuthor && (
                 <button
                     className="charplanner__delete button"
