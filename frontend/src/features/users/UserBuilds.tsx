1c374d44999fd45df5188b1e5be35a9607ab1040
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2024-09-21 04:51:41 +0200

    Adds table skeleton to BuildsTable and UserBuilds

--- frontend/src/features/users/UserBuilds.tsx
+++ frontend/src/features/users/UserBuilds.tsx
@@ -201,78 +201,111 @@ const UserBuilds = ({
                     </thead>
                 }
                 <tbody>
-                    {isLoading ?
-                        <tr className="table--placeholder">
-                            <td
-                                className="cliploader"
-                                style={{
-                                    width: "50px",
-                                    height: "50px",
-                                }}
-                                aria-label="Loading Spinner"
-                                data-testid="loader"
-                            >
-                                <div
-                                    className="cliploader-section"
-                                    style={{
-                                        borderTopColor: "rgb(231, 214, 182)",
-                                        borderLeftColor: "rgb(231, 214, 182)",
-                                        borderBottomColor: "rgb(231, 214, 182)",
-                                        animationDuration: "1.5s",
-                                    }}
-                                />
-                            </td>
-                        </tr>
-                        : isError
-                            ?
-                            <tr className="table--placeholder">
-                                <td className="sm-alert errmsg">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                    <span>Error fetching builds</span>
-                                </td>
-                            </tr>
-                            : !data || data.builds.length === 0
-                                ?
-                                <tr className="table--placeholder">
-                                    <td>No Builds found</td>
+                    {(isLoading || isError || (!data || data?.builds?.length === 0)) ?
+                        <>
+                            {!isMobile && Array(table.getState().pagination.pageSize).fill(null).map((_el, idx) => (
+                                <tr
+                                    key={idx}
+                                    className="table__row build"
+                                >
+                                    {columns.map((_el, idx) => (
+                                        <td
+                                            key={idx}
+                                            className={`table__cell`}
+                                        >
+                                            <p className={`paragraph-skeleton${isLoading ? " animated" : ""}`}>
+                                            </p>
+                                        </td>
+                                    ))}
                                 </tr>
-                                :
-                                <>
-                                    {table.getRowModel().rows.map(row => {
-                                        return (
-                                            <tr key={row.id} className="table__row build">
-                                                {row.getVisibleCells().map(cell => {
-                                                    if (isMobile) {
-                                                        const header = capitalizeFirstLetter(cell.column.id);
+                            ))}
+                            {!isMobile && !isLoading &&
+                                <tr className="table__errmsg">
+                                    {isError ?
+                                        <td className="sm-alert errmsg">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                            <span>Error fetching builds</span>
+                                        </td>
+                                        :
+                                        <td className="sm-alert bg-color">
+                                            <span>No Builds found</span>
+                                        </td>
+                                    }
+                                </tr>
+                            }
+                            {isMobile &&
+                                <tr className="table--placeholder">
+                                    {isLoading ?
+                                        <td
+                                            className="cliploader"
+                                            style={{
+                                                width: "50px",
+                                                height: "50px",
+                                            }}
+                                            aria-label="Loading Spinner"
+                                            data-testid="loader"
+                                        >
+                                            <div
+                                                className="cliploader-section"
+                                                style={{
+                                                    borderTopColor: "rgb(231, 214, 182)",
+                                                    borderLeftColor: "rgb(231, 214, 182)",
+                                                    borderBottomColor: "rgb(231, 214, 182)",
+                                                    animationDuration: "1.5s",
+                                                }}
+                                            />
+                                        </td>
+                                        :
+                                        isError ?
+                                            <td className="sm-alert errmsg">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                                <span>Error fetching builds</span>
+                                            </td>
+                                            :
+                                            <td className="sm-alert bg-color">
+                                                <span>No Builds found</span>
+                                            </td>
+                                    }
+                                </tr>
+                            }
+                        </>
+                        :
+                        <>
+                            {table.getRowModel().rows.map(row => {
+                                return (
+                                    <tr key={row.id} className="table__row build">
+                                        {row.getVisibleCells().map(cell => {
+                                            if (isMobile) {
+                                                const header = capitalizeFirstLetter(cell.column.id);
 
-                                                        return (
-                                                            <td key={cell.id} className={`table__cell ${cell.column.id}`}>
-                                                                <div className="table__cell__head">
-                                                                    {header}:
-                                                                </div>
-                                                                <div className="table__cell__body">
-                                                                    {flexRender(
-                                                                        cell.column.columnDef.cell,
-                                                                        cell.getContext()
-                                                                    )}
-                                                                </div>
-                                                            </td>
-                                                        )
-                                                    } else {
-                                                        return (
-                                                            <td key={cell.id} className={`table__cell ${cell.column.id}`}>
-                                                                {flexRender(
-                                                                    cell.column.columnDef.cell,
-                                                                    cell.getContext()
-                                                                )}
-                                                            </td>
-                                                        )
-                                                    }
-                                                })}
-                                            </tr>
-                                        )
-                                    })}
-                                </>
+                                                return (
+                                                    <td key={cell.id} className={`table__cell ${cell.column.id}`}>
+                                                        <div className="table__cell__head">
+                                                            {header}:
+                                                        </div>
+                                                        <div className="table__cell__body">
+                                                            {flexRender(
+                                                                cell.column.columnDef.cell,
+                                                                cell.getContext()
+                                                            )}
+                                                        </div>
+                                                    </td>
+                                                )
+                                            } else {
+                                                return (
+                                                    <td key={cell.id} className={`table__cell ${cell.column.id}`}>
+                                                        {flexRender(
+                                                            cell.column.columnDef.cell,
+                                                            cell.getContext()
+                                                        )}
+                                                    </td>
+                                                )
+                                            }
+                                        })}
+                                    </tr>
+                                )
+                            })}
+                        </>
                     }
                 </tbody>
             </table>
