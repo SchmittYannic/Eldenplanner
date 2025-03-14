83404aa008d08eb2d2313f4ff98563b1611a1e62
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-03-14 22:41:20 +0100

    Removes vite-plugin-rewrite-all

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -16,7 +15,6 @@ export default defineConfig(({ command, mode }) => {
     },
     plugins: [
       react(),
-      pluginRewriteAll(),
     ],
     resolve: {
       alias: {
