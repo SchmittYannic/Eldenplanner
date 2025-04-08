007833f5a0ecee9550335261d23646b115944ecd
SchmittYannic <56793005+SchmittYannic@users.noreply.github.com>
2025-04-08 15:30:53 +0200

    Adds back to homepage button to Impressum

--- frontend/src/components/Impressum.tsx
+++ frontend/src/components/Impressum.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Impressum.scss"
 
 const Impressum = () => {
@@ -33,6 +34,16 @@ const Impressum = () => {
                         </a>
                     </p>
                 </div>
+                <Link
+                    className={`button`}
+                    to="/"
+                >
+                    <svg className="mr-2" width="12" height="12" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z" fill="currentColor" />
+                    </svg>
+
+                    <span>Back to homepage</span>
+                </Link>
             </div>
         </main>
     )
